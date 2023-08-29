a2e35d63b42e83584e8e76c5fe83bb3153fe9221
Fabian <fabian@nostur.com>
2023-08-30 01:30:20 +0700

    Color themes: Add NRButton to use instead of .borderedProminent

---
+++ Nostur/ViewFragments/NRButton.swift
@@ -0,0 +1,51 @@
+//
+//  NRButton.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 29/08/2023.
+//
+
+import SwiftUI
+
+struct NRButtonStyle: ButtonStyle {
+    var theme:Theme
+    var style:Style = .default
+    
+    enum Style {
+        case `default`
+        case borderedProminent
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        switch style {
+        case .default:
+            configuration.label
+                .foregroundColor(theme.accent)
+        case .borderedProminent:
+            configuration.label
+                .padding(.horizontal, 12)
+                .padding(.vertical, 6)
+                .background(theme.accent)
+                .cornerRadius(5)
+                .foregroundColor(Color.white)
+        }
+    }
+}
+
+struct NRButton_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HStack {
+                Button("Example") { }
+                    .buttonStyle(.borderedProminent)
+                
+                Button("Example") { }
+                    .buttonStyle(NRButtonStyle(theme: Theme.default, style: .borderedProminent))
+            }
+                
+                
+        }
+        .buttonStyle(NRButtonStyle(theme: Theme.default))
+        .previewDevice(PreviewDevice(rawValue: "iPhone 14"))
+    }
+}
