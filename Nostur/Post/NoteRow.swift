451bfe850a7b3389f8cdeab8841b3fc55a65998e
Fabian <fabian@nostur.com>
2023-08-30 22:40:36 +0700

    add .debugDimensions() for debugging view dimensions

--- Nostur/Post/NoteRow.swift
+++ Nostur/Post/NoteRow.swift
@@ -123,18 +123,6 @@ struct NoteRow: View {
     }
 }
 
-extension View {
-  func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
-    overlay(
-      GeometryReader { geometryProxy in
-        Color.clear
-          .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
-      }
-    )
-    .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
-  }
-}
-
 enum ThreadConnectDirection {
     case top
     case bottom
--- Nostur/Utils/View+centered.swift
+++ Nostur/Utils/View+centered.swift
@@ -42,6 +42,39 @@ struct HorizontallyCenteredView: ViewModifier {
 }
 
 extension View {
+  func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+    overlay(
+      GeometryReader { geometryProxy in
+        Color.clear
+          .preference(key: SizePreferenceKey.self, value: geometryProxy.size)
+      }
+    )
+    .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+  }
+}
+
+struct DebugDimensions: ViewModifier {
+    
+    @State var actualSize:CGSize? = nil
+    
+    func body(content: Content) -> some View {
+        content
+            .readSize { size in
+                actualSize = size
+            }
+            .overlay(alignment: .bottomTrailing) {
+                if let actualSize {
+                    Text(actualSize.debugDescription)
+                        .font(.caption)
+                        .foregroundColor(.white)
+                        .background(.black)
+                        .fontWeight(.bold)
+                }
+            }
+    }
+}
+
+extension View {
     func centered() -> some View {
         modifier(CenteredView())
     }
@@ -50,5 +83,8 @@ extension View {
     }
     func hCentered() -> some View {
         modifier(HorizontallyCenteredView())
+    }
+    func debugDimensions() -> some View {
+        modifier(DebugDimensions())
     }
 }
