afd3d72c20ec341b9b80d8acc4d8c2fa10294dac
Fabian <fabian@nostur.com>
2023-09-08 21:27:40 +0700

    App theme selector: show which theme is selected

--- Nostur/Mainfeed/FeedSettings+AppThemeSwitcher.swift
+++ Nostur/Mainfeed/FeedSettings+AppThemeSwitcher.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct AppThemeSwitcher: View {
+    @AppStorage("app_theme") var selectedTheme = "default"
     @Binding var showFeedSettings:Bool
     
     var body: some View {
@@ -19,42 +20,56 @@ struct AppThemeSwitcher: View {
                 Spacer()
                 Color("defaultAccentColor")
                     .frame(width: 25, height: 25)
+                    .padding(3)
+                    .background(selectedTheme == "default" ? Color.secondary : .clear)
                     .onTapGesture {
                         Theme.default.loadDefault()
                         showFeedSettings = false
                     }
                 Color("purpleAccentColor")
                     .frame(width: 25, height: 25)
+                    .padding(3)
+                    .background(selectedTheme == "purple" ? Color.secondary : .clear)
                     .onTapGesture {
                         Theme.default.loadPurple()
                         showFeedSettings = false
                     }
                 Color("redAccentColor")
                     .frame(width: 25, height: 25)
+                    .padding(3)
+                    .background(selectedTheme == "red" ? Color.secondary : .clear)
                     .onTapGesture {
                         Theme.default.loadRed()
                         showFeedSettings = false
                     }
                 Color("greenAccentColor")
                     .frame(width: 25, height: 25)
+                    .padding(3)
+                    .background(selectedTheme == "green" ? Color.secondary : .clear)
                     .onTapGesture {
                         Theme.default.loadGreen()
                         showFeedSettings = false
                     }
                 Color("blueAccentColor")
                     .frame(width: 25, height: 25)
+                    .padding(3)
+                    .background(selectedTheme == "blue" ? Color.secondary : .clear)
                     .onTapGesture {
                         Theme.default.loadBlue()
                         showFeedSettings = false
                     }
                 Color("pinkAccentColor")
                     .frame(width: 25, height: 25)
+                    .padding(3)
+                    .background(selectedTheme == "pink" ? Color.secondary : .clear)
                     .onTapGesture {
                         Theme.default.loadPink()
                         showFeedSettings = false
                     }
                 Color("orangeAccentColor")
                     .frame(width: 25, height: 25)
+                    .padding(3)
+                    .background(selectedTheme == "orange" ? Color.secondary : .clear)
                     .onTapGesture {
                         Theme.default.loadOrange()
                         showFeedSettings = false
