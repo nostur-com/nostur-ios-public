93633c5201c4f96cf4bf8c38e1565b7964e11d1c
Fabian <fabian@nostur.com>
2023-09-17 23:24:52 +0700

    Refactor: use FetchVM for following count

---
+++ Nostur/Profiles/ProfileFollowingCount.swift
@@ -0,0 +1,52 @@
+//
+//  ProfileFollowingCount.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 17/09/2023.
+//
+
+import SwiftUI
+
+struct ProfileFollowingCount: View {
+    let pubkey:String
+    
+    @StateObject private var vm = FetchVM<Int>()
+
+    var body: some View {
+        switch vm.state {
+        case .initializing:
+            Text("Following \(Image(systemName: "hourglass.circle.fill"))", comment: "Label for Following count")
+                .onAppear {
+                    vm.setFetchParams((
+                        req: {
+                            bg().perform { // 1. FIRST CHECK LOCAL DB
+                                if let clEvent = Event.fetchReplacableEvent(3, pubkey: pubkey, context: bg()) {
+                                    vm.ready(clEvent.pTags().count)
+                                }
+                                else { req(RM.getAuthorContactsList(pubkey: pubkey)) }
+                            }
+                        }, 
+                        onComplete: { relayMessage in
+                            bg().perform { // 3. WE SHOULD HAVE IT IN LOCAL DB NOW
+                                if let clEvent = Event.fetchReplacableEvent(3, pubkey: pubkey, context: bg()) {
+                                    vm.ready(clEvent.pTags().count)
+                                }
+                                else { vm.timeout() }
+                            }
+                        }
+                    ))
+                    vm.fetch()
+                }
+        case .loading:
+            Text("\(Image(systemName: "hourglass.circle.fill")) Following", comment: "Label for Following count")
+        case .ready(let count):
+            Text("\(count) Following", comment: "Label for Following count")
+        case .timeout:
+            Text("\(Image(systemName: "person.fill.questionmark")) Following", comment: "Label for Following count")
+                .onTapGesture {
+                    vm.state = .loading
+                    vm.fetch()
+                }
+        }
+    }
+}
