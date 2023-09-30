60fbc111d60230368c92565c79a5b8569ff6d23f
Fabian <fabian@nostur.com>
2023-09-30 19:27:14 +0700

    Notification toggles: add new editable Replying to view to replace old "Replying to.." preview when composing reply
    - ReplyingToEditable renders "Replying to @bla and @blo"
    - ContactsToggleSheet allows toggling who to send notifications to

---
+++ Nostur/Post/PostComposer/ContactsToggleSheet.swift
@@ -0,0 +1,117 @@
+//
+//  ContactsToggleSheet.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 30/09/2023.
+//
+
+import SwiftUI
+
+struct ContactsToggleSheet: View {
+    @Environment(\.dismiss) private var dismiss
+    public var requiredP:String? = nil
+    public var available:Set<Contact>
+    @Binding public var selected:Set<Contact>
+    
+    private var contactList:[Contact] {
+        Array(available)
+            .sorted(by: { $0.pubkey == requiredP && $1.pubkey != requiredP })
+    }
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                Divider()
+                LazyVStack(alignment: .leading, spacing: 10) {
+                    // if we don't have requiredP in contactList, render placeholder here:
+                    
+                    
+                    // contacts to toggle (but disable toggle for requiredP)
+                    ForEach(contactList) { contact in
+                        HStack(spacing: 10) {
+                            Button {
+                                guard contact.pubkey != requiredP else { return }
+                                if selected.contains(contact) {
+                                    selected.remove(contact)
+                                }
+                                else {
+                                    selected.insert(contact)
+                                }
+                            } label: {
+                                if selected.contains(contact) {
+                                    Image(systemName:  "checkmark.circle.fill")
+                                        .padding(.vertical, 10)
+                                }
+                                else {
+                                    Image(systemName:  "circle")
+                                        .foregroundColor(Color.secondary)
+                                        .padding(.vertical, 10)
+                                }
+                            }
+                            Text(contact.anyName)
+//                                .padding(.vertical, 10)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            if contact.pubkey == requiredP {
+                                Spacer()
+                                Text("required")
+                                    .font(.system(size: 12.0))
+                                    .italic()
+                            }
+                        }
+                        .contentShape(Rectangle())
+                        .disabled(contact.pubkey == requiredP)
+                        .opacity(contact.pubkey == requiredP ? 0.5 : 1.0)
+                        .onTapGesture {
+                            guard contact.pubkey != requiredP else { return }
+                            if selected.contains(contact) {
+                                selected.remove(contact)
+                            }
+                            else {
+                                selected.insert(contact)
+                            }
+                        }
+                        Divider()
+                    }
+                    
+                }
+            }
+            .navigationTitle("Notify selection (\(selected.count))")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                if available.count > 5 {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        if selected.count == available.count {
+                            Image(systemName: "checklist.checked")
+                                .onTapGesture {
+                                    selected = available.filter { $0.pubkey == requiredP }
+                                }
+                        }
+                        else {
+                            Image(systemName: "checklist.unchecked")
+                                .onTapGesture {
+                                    selected = available
+                                }
+                        }
+                    }
+                }
+                
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Done") {
+                        dismiss()
+                    }
+                }
+            }
+            .padding(10)
+        }
+    }
+}
+
+#Preview("Replying to selector") {
+    PreviewContainer({ pe in
+        pe.loadContacts()
+    }) {
+        NavigationStack {
+            ReplyingToEditableTester()
+        }
+    }
+}
---
+++ Nostur/Post/PostComposer/ReplyingToEditable.swift
@@ -0,0 +1,73 @@
+//
+//  ReplyingToEditable.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 30/09/2023.
+//
+
+import SwiftUI
+
+struct ReplyingToEditable: View {
+    @Environment(\.dismiss) private var dismiss
+    @EnvironmentObject var theme: Theme
+    public var requiredP:String? = nil
+    public var available:Set<Contact>
+    @Binding var selected:Set<Contact>
+    
+    private var selectedSorted:[Contact] {
+        Array(selected)
+            .sorted(by: { $0.pubkey == requiredP && $1.pubkey != requiredP })
+    }
+    
+    @State private var showContactsToggleSheet = false
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 0) {
+            Group {
+                Text("Replying to ")
+                    .foregroundColor(theme.secondary)
+                    .lineLimit(1)
+                Text(selectedSorted.map { "@\($0.anyName)" }.formatted(.list(type: .and)))
+                    .foregroundColor(theme.accent)
+                    .lineLimit(3)
+            }
+            .font(.system(size: 13))
+            .fontWeight(.light)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            guard available.count > 1 else { return } // always require at least one .p or we dont show the toggle sheet
+            showContactsToggleSheet = true
+        }
+        .sheet(isPresented: $showContactsToggleSheet) {
+            ContactsToggleSheet(requiredP: requiredP, available: available, selected: $selected)
+            .presentationDetents([.medium])
+        }
+    }
+}
+
+struct ReplyingToEditableTester: View {
+    @State private var requiredP:String? = nil
+    @State private var available:Set<Contact> = []
+    @State private var selected:Set<Contact> = []
+    
+    var body: some View {
+        ReplyingToEditable(requiredP: requiredP, available: available, selected: $selected)
+            .onAppear {
+                let contacts = PreviewFetcher.allContacts()
+                available = Set(contacts.prefix(6))
+                selected = Set(contacts.prefix(6))
+            }
+    }
+}
+
+
+#Preview("Replying to selector") {
+    PreviewContainer({ pe in
+        pe.loadContacts()
+    }) {
+        NavigationStack {
+            ReplyingToEditableTester()
+        }
+    }
+}
