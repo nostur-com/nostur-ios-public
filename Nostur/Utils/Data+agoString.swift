709b55ae8330ec4260483ec841b040a4807114f4
Fabian <fabian@nostur.com>
2023-10-01 21:40:09 +0700

    Optimize .agoString again
    - removed no effect ChatGPT optimization
    - static string localization

--- Nostur/Utils/Data+agoString.swift
+++ Nostur/Utils/Data+agoString.swift
@@ -8,21 +8,29 @@
 import Foundation
 
 extension Date {
-    static let timeUnits: [(Double, String)] = [
-        (365.0 * 24.0 * 60.0 * 60.0, "%dy"),
-        (24.0 * 60.0 * 60.0, "%dd"),
-        (60.0 * 60.0, "%dh"),
-        (60.0, "%dm"),
-        (1.0, "%ds")
-    ]
+    
+    static let yearString = String(localized: "y", comment: "short for year (ago), appended to number. Example: 1y")
+    static let dayString = String(localized: "d", comment: "short for days (ago), appended to number. Example: 2d")
+    static let hourString = String(localized: "h", comment: "short for hours (ago), appended to number. Example: 7h")
+    static let minuteString = String(localized: "m", comment: "short for minutes (ago), appended to number. Example: 3m")
+    static let secondString = String(localized: "s", comment: "short for seconds (ago), appended to number. Example: 15s")
+    
+    static let secondsInMinute = 60.0
+    static let secondsInHour = secondsInMinute * 60.0
+    static let secondsInDay = secondsInHour * 24.0
+    static let secondsInYear = secondsInDay * 365.0
     
     var agoString: String {
-        let interval = -timeIntervalSinceNow
-        for (unitSeconds, format) in Self.timeUnits {
-            if interval >= unitSeconds {
-                return String.localizedStringWithFormat(format, Int(interval / unitSeconds))
-            }
+        if -timeIntervalSinceNow >= Self.secondsInYear {
+            return (String(Int(-timeIntervalSinceNow / Self.secondsInYear)) + Self.yearString)
+        } else if -timeIntervalSinceNow >= Self.secondsInDay {
+            return (String(Int(-timeIntervalSinceNow / Self.secondsInDay)) + Self.dayString)
+        } else if -timeIntervalSinceNow >= Self.secondsInHour {
+            return (String(Int(-timeIntervalSinceNow / Self.secondsInHour)) + Self.hourString)
+        } else if -timeIntervalSinceNow >= Self.secondsInMinute {
+            return (String(Int(-timeIntervalSinceNow / Self.secondsInMinute)) + Self.minuteString)
+        } else {
+            return (String(max(1,Int(-timeIntervalSinceNow / Self.secondsInDay))) + Self.secondString)
         }
-        return "now"
     }
 }
