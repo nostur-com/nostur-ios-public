7426ee6c79f4dd66c7165ff4c47f3628384e6099
Fabian <fabian@nostur.com>
2023-11-01 21:28:09 +0100

    set lastReadId on tap instead of after scroll
    maybe fixes auto scroll sometimes not working

--- Nostur/Mainfeed/SmoothList.swift
+++ Nostur/Mainfeed/SmoothList.swift
@@ -77,7 +77,7 @@ struct SmoothList: UIViewControllerRepresentable {
                     L.og.error("🔴🔴 not scrolling: index+1: \(index+1) is > than cvh.dataSource?.snapshot().numberOfItems(inSection: .main)")
                     return
                 }
-                
+                context.coordinator.lvm.lastReadId = context.coordinator.lvm.posts.elements[index].value.id
                 cvh.collectionView.scrollToItem(at: IndexPath(item: max(0,index), section: 0), at: .top, animated: true)
             }
             .store(in: &context.coordinator.subscriptions)
