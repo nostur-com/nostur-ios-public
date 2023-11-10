63abd2df46a4f5d08b001af069aeb1900bbc8b6b
Fabian <fabian@nostur.com>
2023-11-10 21:03:59 +0100

    iCloud sync: put Accounts into own iCloud synced table
    (Account -> CloudAccount)

---
+++ Nostur/CoreData/CloudAccount+CoreDataClass.swift
@@ -0,0 +1,55 @@
+//
+//  CloudAccount+CoreDataClass.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 06/11/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CloudAccount)
+public class CloudAccount: NSManagedObject {
+    var noPrivateKey = false
+ 
+    public func getFollowingPFPs() -> [String: URL] {
+        return Dictionary(grouping: follows) { contact in
+            contact.pubkey
+        }
+        .compactMapValues({ contacts in
+            guard let picture = contacts.first?.picture else { return nil }
+            guard picture.prefix(7) != "http://" else { return nil }
+            return URL(string: picture)
+        })
+    }
+    
+    public func getFollowingPublicKeys(includeBlocked:Bool = false) -> Set<String> {
+        let withSelfIncluded = Set([publicKey]).union(followingPubkeys)
+        if includeBlocked {
+            return withSelfIncluded
+        }
+        let withoutBlocked = withSelfIncluded.subtracting(NRState.shared.blockedPubkeys)
+        return withSelfIncluded
+    }
+    
+    public func getSilentFollows() -> Set<String> {
+        return Set(follows.filter { $0.privateFollow }.map { $0.pubkey })
+    }
+    
+    public func publishNewContactList() {
+        guard let clEvent = try? AccountManager.createContactListEvent(account: self)
+        else {
+            L.og.error("🔴🔴 Could not create new clEvent")
+            return
+        }
+        if self.isNC {
+            NSecBunkerManager.shared.requestSignature(forEvent: clEvent, usingAccount: self, whenSigned: { signedEvent in
+                _ = Unpublisher.shared.publishLast(signedEvent, ofType: .contactList)
+            })
+        }
+        else {
+            _ = Unpublisher.shared.publishLast(clEvent, ofType: .contactList)
+        }
+    }
+}
---
+++ Nostur/CoreData/CloudAccount+CoreDataProperties.swift
@@ -0,0 +1,330 @@
+//
+//  CloudAccount+CoreDataProperties.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 06/11/2023.
+//
+//
+
+import Foundation
+import CoreData
+import CloudKit
+
+extension CloudAccount {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CloudAccount> {
+        return NSFetchRequest<CloudAccount>(entityName: "CloudAccount")
+    }
+
+    @NSManaged public var about_: String?
+    @NSManaged public var banner_: String?
+    @NSManaged public var blockedPubkeys: String?
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var display_name_: String?
+    @NSManaged public var flags: String?
+    @NSManaged public var followingHashtags_: String?
+    @NSManaged public var followingPubkeys_: String?
+    @NSManaged public var isNC: Bool
+    @NSManaged public var lastFollowerCreatedAt: Int64
+    @NSManaged public var lastProfileReceivedAt: Date?
+    @NSManaged public var lastSeenDMRequestCreatedAt: Int64
+    @NSManaged public var lastSeenPostCreatedAt: Int64
+    @NSManaged public var lastSeenReactionCreatedAt: Int64
+    @NSManaged public var lastSeenRepostCreatedAt: Int64
+    @NSManaged public var lastSeenZapCreatedAt: Int64
+    @NSManaged public var lud06_: String?
+    @NSManaged public var lud16_: String?
+    @NSManaged public var mutedRootIds: String?
+    @NSManaged public var name_: String?
+    @NSManaged public var ncRelay_: String?
+    @NSManaged public var nip05_: String?
+    @NSManaged public var picture_: String?
+    @NSManaged public var publicKey_: String?
+    @NSManaged public var lastLoginAt_: Date?
+    
+    // -- MARK: Non-optional getters/setters
+    
+    public var about: String {
+        get { about_ ?? "" }
+        set { about_ = newValue }
+    }
+    
+    public var banner: String {
+        get { banner_ ?? "" }
+        set { banner_ = newValue }
+    }
+    
+    public var display_name: String {
+        get { display_name_ ?? "" }
+        set { display_name_ = newValue }
+    }
+    
+    public var pictureUrl:URL? {
+        guard let picture = picture_ else { return nil }
+        return URL(string: picture)
+    }
+    
+    var followingHashtags:Set<String> {
+        get {
+            guard let followingHashtags_ else { return [] }
+            return Set(followingHashtags_.split(separator: " ").map { String($0) })
+        }
+        set {
+            followingHashtags_ = newValue.joined(separator: " ")
+        }
+    }
+    
+    var follows:[Contact] {
+        get {
+            guard let followingPubkeys = followingPubkeys_ else { return [] }
+            return Contact.fetchByPubkeys(followingPubkeys.split(separator: " ").map { String($0) }, context: Thread.isMainThread ? DataProvider.shared().viewContext : bg())
+        }
+        set {
+            followingPubkeys_ = newValue.map { $0.pubkey }.joined(separator: " ")
+        }
+    }
+    
+    var followingPubkeys:Set<String> {
+        get {
+            guard let followingPubkeys = followingPubkeys_ else { return [] }
+            return Set(followingPubkeys.split(separator: " ").map { String($0) })
+        }
+        set {
+            followingPubkeys_ = newValue.joined(separator: " ")
+        }
+    }
+    
+    public var lud06: String {
+        get { lud06_ ?? "" }
+        set { lud06_ = newValue }
+    }
+    
+    public var lud16: String {
+        get { lud16_ ?? "" }
+        set { lud16_ = newValue }
+    }
+    
+    public var name: String {
+        get { name_ ?? "" }
+        set { name_ = newValue }
+    }
+    
+    public var ncRelay: String {
+        get { ncRelay_ ?? "" }
+        set { ncRelay_ = newValue }
+    }
+    
+    public var nip05: String {
+        get { nip05_ ?? "" }
+        set { nip05_ = newValue }
+    }
+    
+    public var picture: String {
+        get { picture_ ?? "" }
+        set { picture_ = newValue }
+    }
+    
+    public var publicKey: String {
+        get { publicKey_ ?? "" }
+        set { publicKey_ = newValue }
+    }
+    
+    public var mostRecentItemDate:Int64 { // Used for duplicate accounts in iCloud, to resolve which one to keep (most recent)
+        return [
+            Int64((createdAt ?? .distantPast).timeIntervalSince1970),
+            lastSeenRepostCreatedAt,
+            lastSeenPostCreatedAt,
+            lastSeenZapCreatedAt,
+            lastSeenReactionCreatedAt,
+            lastFollowerCreatedAt,
+            Int64((lastProfileReceivedAt ?? .distantPast).timeIntervalSince1970),
+            lastSeenDMRequestCreatedAt,
+            
+        ].sorted(by: >).first!
+    }
+
+    public var lastLoginAt: Date {
+        get { lastLoginAt_ ?? .distantPast }
+        set { lastLoginAt_ = newValue }
+    }
+    
+}
+
+extension CloudAccount : Identifiable {
+
+    var anyName:String {
+        if name != "" { return name }
+        if display_name != "" { return display_name }
+        return String(npub.prefix(11))
+    }
+    
+    var npub:String { try! NIP19(prefix: "npub", hexString: publicKey).displayString }
+    
+    var privateKey:String? {
+        get {
+            if noPrivateKey { return nil }
+            if isNC {
+                if let key = NIP46SecretManager.shared.getSecret(account: self) {
+                    noPrivateKey = false
+                    return key
+                }
+                return nil
+            }
+            
+            if let key = AccountManager.shared.getPrivateKeyHex(pubkey: self.publicKey, account: self) {
+                noPrivateKey = false
+                return key
+            }
+            else {
+                return nil
+            }
+        }
+        set(privateKeyHex) {
+            guard privateKeyHex != nil else {
+                AccountManager.shared.deletePrivateKey(forPublicKeyHex: self.publicKey)
+                return
+            }
+            noPrivateKey = false
+            AccountManager.shared.storePrivateKey(privateKeyHex: privateKeyHex!, forPublicKeyHex: self.publicKey)
+        }
+    }
+    
+    var nsec:String? {
+        get {
+            guard self.privateKey != nil else { return nil }
+            guard let nsec = try? NIP19(prefix: "nsec", hexString: self.privateKey!).displayString else {
+                return nil
+            }
+            return nsec
+        }
+    }
+    
+    
+    // For when adding read only accounts, prefill with kind.0 info from relays (FROM CACHE)
+    static func preFillReadOnlyAccountInfo(account:CloudAccount, context:NSManagedObjectContext, forceOverwrite:Bool = false) {
+        
+        guard let kind0 = Event.setMetaDataEvents(byAuthorPubkey: account.publicKey, context: context)?.first else {
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        guard let metaData = try? decoder.decode(NSetMetadata.self, from: kind0.content!.data(using: .utf8, allowLossyConversion: false)!) else {
+            return
+        }
+
+        if (account.privateKey == nil || forceOverwrite) { // Don't overwrite non-read-only accounts
+            account.objectWillChange.send()
+//            account.display_name = metaData.display_name ?? ""
+            account.name = metaData.name ?? ""
+            account.about = metaData.about ?? ""
+            account.picture = metaData.picture ?? ""
+            account.banner = metaData.banner ?? ""
+            account.nip05 = metaData.nip05 ?? ""
+            account.lud16 = metaData.lud16 ?? ""
+            account.lud06 = metaData.lud06 ?? ""
+        }
+    }
+    
+    // For when adding read only accounts, prefill with kind.0 info from relays (NEW EVENT FROM IMPORTER)
+    static func preFillReadOnlyAccountInfo(event:NEvent, context:NSManagedObjectContext, forceOverwrite:Bool = false) {
+        
+        let decoder = JSONDecoder()
+        guard let metaData = try? decoder.decode(NSetMetadata.self, from: event.content.data(using: .utf8, allowLossyConversion: false)!) else {
+            return
+        }
+        
+        let fr = CloudAccount.fetchRequest()
+        fr.predicate = NSPredicate(format: "publicKey_ = %@", event.publicKey)
+        if let account = try? context.fetch(fr).first {
+            if (account.privateKey == nil || forceOverwrite == true) { // Don't overwrite non-read-only accounts
+                account.objectWillChange.send()
+//                account.display_name = metaData.display_name ?? ""
+                account.name = metaData.name ?? ""
+                account.about = metaData.about ?? ""
+                account.picture = metaData.picture ?? ""
+                account.banner = metaData.banner ?? ""
+                account.nip05 = metaData.nip05 ?? ""
+                account.lud16 = metaData.lud16 ?? ""
+                account.lud06 = metaData.lud06 ?? ""
+            }
+        }
+    }
+    
+    // For when adding read only accounts, prefill with kind.3 info from relays (FROM CACHE)
+    static func preFillReadOnlyAccountFollowing(account:CloudAccount, context:NSManagedObjectContext) {
+        
+        guard let kind3 = Event.contactListEvents(byAuthorPubkey: account.publicKey, context: context)?.first else {
+            return
+        }
+        
+        let contacts = Contact.ensureContactsCreated(event: kind3.toNEvent(), context: context, limit:999)
+        
+        // if read only account, import follows. Or pendingFirstContactsFetch
+        if (!contacts.isEmpty) {
+//            account.objectWillChange.send()
+            for contact in contacts {
+                account.followingPubkeys.insert(contact.pubkey)
+            }
+        }
+    }
+    
+    func toBG() -> CloudAccount? {
+        bg().object(with: self.objectID) as? CloudAccount
+    }
+    
+    
+    func signEvent(_ event:NEvent) throws -> NEvent {
+        guard let privateKey = self.privateKey else {
+            L.og.error("🔴🔴🔴🔴🔴 private key missing, could not sign 🔴🔴🔴🔴🔴")
+            throw "account or keys missing, could not sign"
+        }
+        
+        var eventToSign = event
+        do {
+            let keys = try NKeys(privateKeyHex: privateKey)
+            let signedEvent = try eventToSign.sign(keys)
+            return signedEvent
+        }
+        catch {
+            L.og.error("🔴🔴🔴🔴🔴 Could not sign event 🔴🔴🔴🔴🔴")
+            throw "Could not sign event"
+        }
+    }
+    
+    func signEventBg(_ event:NEvent) throws -> NEvent {
+        guard let account = self.toBG() else {
+            L.og.error("🔴🔴🔴🔴🔴 Acccount missing, could not sign 🔴🔴🔴🔴🔴")
+            throw "account missing, could not sign"
+        }
+        guard let pk = account.privateKey else {
+            L.og.error("🔴🔴🔴🔴🔴 private key missing, could not sign 🔴🔴🔴🔴🔴")
+            throw "keys missing, could not sign"
+        }
+        
+        var eventToSign = event
+        do {
+            let keys = try NKeys(privateKeyHex: pk)
+            let signedEvent = try eventToSign.sign(keys)
+            return signedEvent
+        }
+        catch {
+            L.og.error("🔴🔴🔴🔴🔴 Could not sign event 🔴🔴🔴🔴🔴")
+            throw "Could not sign event"
+        }
+    }
+    
+    static func fetchAccount(publicKey:String, context:NSManagedObjectContext) throws -> CloudAccount? {
+        let request = NSFetchRequest<CloudAccount>(entityName: "CloudAccount")
+        request.predicate = NSPredicate(format: "publicKey_ = %@", publicKey)
+        request.fetchLimit = 1
+        request.fetchBatchSize = 1
+        return try context.fetch(request).first
+    }
+    
+    static func fetchAccounts(context:NSManagedObjectContext) -> [CloudAccount] {
+        let request = NSFetchRequest<CloudAccount>(entityName: "CloudAccount")
+        request.predicate = NSPredicate(value: true)
+        return (try? context.fetch(request)) ?? []
+    }
+    
+}
