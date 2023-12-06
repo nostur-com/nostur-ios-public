2a337f7fef339185be25683d42a8f93b3c0a524a
Fabian <fabian@nostur.com>
2023-12-06 20:38:27 +0100

    Clean up

--- Nostur/Nostr/Importer.swift
+++ Nostur/Nostr/Importer.swift
@@ -96,7 +96,6 @@ class Importer {
     func preloadExistingIdsCache() {
         let fr = Event.fetchRequest()
         fr.fetchLimit = 1_000_000
-//        fr.sortDescriptors = [NSSortDescriptor(keyPath: \Event.created_at, ascending: false)]
         fr.propertiesToFetch = ["id", "relays"]
         
         if let results = try? DataProvider.shared().viewContext.fetch(fr) {
@@ -108,15 +107,6 @@ class Importer {
                 L.og.debug("\(self.existingIds.count) existing ids added to cache")
             }
         }
-        
-//        bg().performAndWait { [unowned self] in // AndWait because existingIds MUST be in sync with db
-//            if let results = try? DataProvider.shared().bg.fetch(fr) {
-//                self.existingIds = results.reduce(into: [String: EventState]()) { (dict, event) in
-//                    dict[event.id] = EventState(status: .SAVED, relays: event.relays)
-//                }
-//                L.og.debug("\(self.existingIds.count) existing ids added to cache")
-//            }
-//        }
     }
     
     public func importEvents() {
