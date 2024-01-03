d8b5a6c0799d9579270926ae297e6eea44891c3d
Fabian <fabian@nostur.com>
2024-01-03 23:07:45 +0100

    NIP-89: Add UnkownKindModel for fetching app handlers and follows recommendations
    - sorts handlers by recommendations
    - uses only web-handlers for now. need to figure out how to handle download app or deeplink

---
+++ Nostur/Post/NR/Kinds/NIP-89/UnknownKindModel.swift
@@ -0,0 +1,244 @@
+//
+//  UnknownKindModel.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 03/01/2024.
+//
+
+import Foundation
+import NostrEssentials
+import CoreData
+
+// UnknownKindModel:
+// Fetches all handlers (31990)
+// Fetches recommendations (31989) from follows
+// Builds a list of suggested apps sorted by recommendations count
+// TODO: Only uses web-handlers for now until we know how when/how to route/redirect to app store download or app deeplink)
+class UnknownKindModel: ObservableObject {
+    @Published var state: ViewState = .loading
+    
+    enum ViewState {
+        case loading
+        case ready(([SuggestedApp],String))
+        case timeout
+    }
+    
+    private let backlog = Backlog(auto: true)
+    private var didBuildSuggestedApps = false
+    private var appRecommendations: [Event] = [] {
+        didSet {
+            if !didBuildSuggestedApps && !appRecommendations.isEmpty && !appHandlers.isEmpty {
+                self.buildSuggestedApps(unknownKind: self.unknownKind!, pubkey: self.pubkey!, eventId: self.eventId!)
+            }
+        }
+    }
+    private var appHandlers: [Event] = [] {
+        didSet {
+            if !didBuildSuggestedApps && !appRecommendations.isEmpty && !appHandlers.isEmpty {
+                self.buildSuggestedApps(unknownKind: self.unknownKind!, pubkey: self.pubkey!, eventId: self.eventId!)
+            }
+        }
+    }
+    private var timeoutCount = 0 {
+        didSet {
+            if timeoutCount >= 2 {
+                state = .timeout
+            }
+        }
+    }
+    
+    // STEP 1 (REQ)
+    private func fetchAppHandlers(unknownKind: Int64) {
+        let reqTask = ReqTask(
+            reqCommand: { taskId in
+                bg().perform {
+                    let filters = [Filters(kinds: [31990], tagFilter: TagFilter(tag: "k", values: ["\(unknownKind)"]), limit: 200)]
+                    
+                    if let reqJson = NostrEssentials.ClientMessage(type: .REQ, subscriptionId: taskId, filters: filters).json() {
+                        req(reqJson)
+                    }
+                }
+            },
+            processResponseCommand: { [weak self]  taskId, _, _ in
+                guard let self = self else { return }
+                bg().perform {
+                    self.appHandlers = self.fetchApps(kind: unknownKind)
+                }
+            },
+            timeoutCommand: { [weak self] taskId in
+                guard let self = self else { return }
+                bg().perform {
+                    self.appHandlers = self.fetchApps(kind: unknownKind)
+                }
+                self.timeoutCount += 1
+            })
+        
+        backlog.add(reqTask)
+        reqTask.fetch()
+    }
+    
+    // STEP 2 (REQ)
+    private func fetchAppRecommendations(unknownKind: Int64) {
+        let reqTask = ReqTask(
+            reqCommand: { [weak self] taskId in
+                guard let self = self else { return }
+                bg().perform {
+                    let filters = [Filters(authors: self.follows!, kinds: [31989], tagFilter: TagFilter(tag: "d", values: ["\(unknownKind)"]), limit: 200)]
+                    
+                    if let reqJson = NostrEssentials.ClientMessage(type: .REQ, subscriptionId: taskId, filters: filters).json() {
+                        req(reqJson)
+                    }
+                }
+            },
+            processResponseCommand: { [weak self] taskId, _, _ in
+                guard let self = self else { return }
+                bg().perform {
+                    self.appRecommendations = self.fetchRecommendations(kind: unknownKind)
+                }
+            },
+            timeoutCommand: { [weak self] taskId in
+                guard let self = self else { return }
+                bg().perform {
+                    self.appRecommendations = self.fetchRecommendations(kind: unknownKind)
+                }
+                self.timeoutCount += 1
+            })
+        
+        backlog.add(reqTask)
+        reqTask.fetch()
+    }
+    
+    // STEP 3A (DB)
+    private func fetchApps(kind: Int64, context: NSManagedObjectContext = context()) -> [Event] {
+        let kindString = "\(kind)"
+        let request = NSFetchRequest<Event>(entityName: "Event")
+        request.predicate = NSPredicate(format: "kind == 31990")
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Event.created_at, ascending: false)]
+        let apps = (try? context.fetch(request)) ?? []
+        
+        return apps.filter {
+            // Only apps which have tag ["k", "<kind here>"]
+            $0.fastTags.first(where: { $0.0 == "k" && $0.1 == kindString }) != nil
+        }
+    }
+    
+    // STEP 3B (DB)
+    private func fetchRecommendations(kind: Int64, context: NSManagedObjectContext = context()) -> [Event] {
+        let kindString = "\(kind)"
+        let request = NSFetchRequest<Event>(entityName: "Event")
+        request.predicate = NSPredicate(format: "kind == 31989 AND pubkey IN %@", self.follows!)
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Event.created_at, ascending: false)]
+        let apps = (try? context.fetch(request)) ?? []
+        
+        return apps.filter {
+            // Only apps which have tag ["k", "<kind here>"]
+            $0.fastTags.first(where: { $0.0 == "d" && $0.1 == kindString }) != nil
+        }
+    }
+    
+    // STEP 4
+    // Take all handlers and recommendations, sort apps by recommendations (only web-handlers for now)
+    private func buildSuggestedApps(unknownKind: Int64, pubkey: String, eventId: String) {
+        bg().perform {
+            let decoder = JSONDecoder()
+            let suggestedApps = self.appHandlers
+                .compactMap({ (handler: Event) -> SuggestedApp? in
+                    guard let content = handler.content else { return nil }
+                    
+                    guard let metaData = try? decoder.decode(NSetMetadata.self, from: content.data(using: .utf8, allowLossyConversion: false)!) else {
+                        return nil
+                    }
+                    
+                    let logoUrl: URL? = if let picture = metaData.picture {
+                        URL(string: picture)
+                    }
+                    else {
+                        nil
+                    }
+                    
+                    guard let webUrl = self.resolveWebUrl(handler: handler) else { return nil }
+                    
+                    return SuggestedApp(
+                        id: handler.aTag,
+                        name: (metaData.name ?? metaData.display_name) ?? "",
+                        description: metaData.about,
+                        logoUrl: logoUrl,
+                        openUrl: webUrl,
+                        recommendedBy: self.appRecommendations.reduce([], { (partialResult: [(Pubkey, URL)], recommendation: Event) in
+                            if let aRef = recommendation.fastTags.first(where: { $0.0 == "a" })?.1,
+                               aRef == handler.aTag,
+                               let pfp = self.followingPFPs![recommendation.pubkey]
+                            {
+                                return partialResult + [(recommendation.pubkey, pfp)]
+                            }
+                            return partialResult
+                        }))
+                })
+                .sorted(by: { $0.recommendedBy.count > $1.recommendedBy.count })
+            
+            DispatchQueue.main.async {
+                self.state = .ready((suggestedApps, self.alt ?? "Unknown post type (kind: \(self.unknownKind!))"))
+                self.didBuildSuggestedApps = true
+            }
+        }
+    }
+    
+    private var unknownKind: Int64?
+    private var eventId: String?
+    private var pubkey: String?
+    private var dTag: String?
+    private var alt: String?
+    private var follows: Set<String>?
+    private var followingPFPs: [Pubkey: URL]?
+    
+    @MainActor
+    public func load(unknownKind: Int64, eventId: String, pubkey: String, dTag: String?, alt: String?) {
+        self.unknownKind = unknownKind
+        self.eventId = eventId
+        self.pubkey = pubkey
+        self.dTag = dTag
+        self.alt = alt
+        self.follows = NRState.shared.loggedInAccount?.followingPublicKeys ?? []
+        self.followingPFPs = NRState.shared.loggedInAccount?.followingPFPs ?? [:]
+        
+        self.fetchAppHandlers(unknownKind: unknownKind)
+        self.fetchAppRecommendations(unknownKind: unknownKind)
+    }
+    
+    // Check if the handler handlers web links and return to URL or nil
+    private func resolveWebUrl(handler: Event) -> URL? {
+        let webHandlers = handler.fastTags.filter { $0.0 == "web" }
+        
+        if let dTag = self.dTag, let naddrHandler = webHandlers.first(where: { $0.2 == "naddr" }) {
+            guard let naddr = try? NostrEssentials.ShareableIdentifier("naddr", kind: Int(self.unknownKind!), pubkey: self.pubkey!, dTag: dTag)
+            else { return nil }
+            guard let webUrl = URL(string: naddrHandler.1.replacingOccurrences(of: "<bech32>", with: naddr.identifier))
+            else { return nil }
+            return webUrl
+        }
+        else if let neventHandler = webHandlers.first(where: { $0.2 == "nevent" }) {
+            guard let nevent = try? NostrEssentials.ShareableIdentifier("nevent", id: self.eventId!)
+            else { return nil }
+            guard let webUrl = URL(string: neventHandler.1.replacingOccurrences(of: "<bech32>", with: nevent.identifier))
+            else { return nil }
+            return webUrl
+        }
+        else if let noteHandler = webHandlers.first { // "note" or handler whatever as note
+            guard let note = try? NostrEssentials.ShareableIdentifier("note", id: self.eventId!)
+            else { return nil }
+            guard let webUrl = URL(string: noteHandler.1.replacingOccurrences(of: "<bech32>", with: note.identifier))
+            else { return nil }
+            return webUrl
+        }
+        return nil
+    }
+}
+
+struct SuggestedApp: Identifiable {
+    public let id: String
+    public let name: String
+    public var description: String?
+    public var logoUrl: URL?
+    public let openUrl: URL
+    public var recommendedBy:[(Pubkey, URL)] = []
+}
