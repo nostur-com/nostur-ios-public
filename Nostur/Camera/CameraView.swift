643fb93c5f0da5da11aa12d23bcab8fd893e107c
Fabian <fabian@nostur.com>
2024-01-27 00:38:55 +0100

    Fix duplicate toolbar buttons
    caused by NBNavigationStack in NBNavigationStack

--- Nostur/Camera/CameraView.swift
+++ Nostur/Camera/CameraView.swift
@@ -81,7 +81,7 @@ struct CameraView: View {
     @StateObject var model = CameraModel()
     
     @State var currentZoomFactor: CGFloat = 1.0
-    private let onUse:(UIImage) -> ()
+    private let onUse: (UIImage) -> ()
     
     init(onUse: @escaping (UIImage) -> ()) {
         self.onUse = onUse
@@ -117,114 +117,111 @@ struct CameraView: View {
     }
     
     var body: some View {
-        NBNavigationStack {
-                GeometryReader { reader in
-                    ZStack {
-                        Color.black.edgesIgnoringSafeArea(.all)
-                        
-                        VStack {
-                            CameraPreview(session: model.session)
-                                .gesture(
-                                    DragGesture().onChanged({ (val) in
-                                        //  Only accept vertical drag
-                                        if abs(val.translation.height) > abs(val.translation.width) {
-                                            //  Get the percentage of vertical screen space covered by drag
-                                            let percentage: CGFloat = -(val.translation.height / reader.size.height)
-                                            //  Calculate new zoom factor
-                                            let calc = currentZoomFactor + percentage
-                                            //  Limit zoom factor to a maximum of 5x and a minimum of 1x
-                                            let zoomFactor: CGFloat = min(max(calc, 1), 5)
-                                            //  Store the newly calculated zoom factor
-                                            currentZoomFactor = zoomFactor
-                                            //  Sets the zoom factor to the capture device session
-                                            model.zoom(with: zoomFactor)
-                                        }
-                                    })
-                                )
-                                .alert(isPresented: $model.showAlertError, content: {
-                                    Alert(title: Text(model.alertError.title), message: Text(model.alertError.message), dismissButton: .default(Text(model.alertError.primaryButtonTitle), action: {
-                                        model.alertError.primaryAction?()
-                                    }))
-                                })
-                                .overlay(
-                                    Group {
-                                        if model.willCapturePhoto {
-                                            Color.black
-                                        }
-                                    }
-                                )
+        GeometryReader { reader in
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    CameraPreview(session: model.session)
+                        .gesture(
+                            DragGesture().onChanged({ (val) in
+                                //  Only accept vertical drag
+                                if abs(val.translation.height) > abs(val.translation.width) {
+                                    //  Get the percentage of vertical screen space covered by drag
+                                    let percentage: CGFloat = -(val.translation.height / reader.size.height)
+                                    //  Calculate new zoom factor
+                                    let calc = currentZoomFactor + percentage
+                                    //  Limit zoom factor to a maximum of 5x and a minimum of 1x
+                                    let zoomFactor: CGFloat = min(max(calc, 1), 5)
+                                    //  Store the newly calculated zoom factor
+                                    currentZoomFactor = zoomFactor
+                                    //  Sets the zoom factor to the capture device session
+                                    model.zoom(with: zoomFactor)
+                                }
+                            })
+                        )
+                        .alert(isPresented: $model.showAlertError, content: {
+                            Alert(title: Text(model.alertError.title), message: Text(model.alertError.message), dismissButton: .default(Text(model.alertError.primaryButtonTitle), action: {
+                                model.alertError.primaryAction?()
+                            }))
+                        })
+                        .overlay(
+                            Group {
+                                if model.willCapturePhoto {
+                                    Color.black
+                                }
+                            }
+                        )
 //                                .animation(.easeInOut)
-                                .opacity(model.photo?.image == nil ? 1.0 : 0)
-                                .overlay {
-                                    if let image = model.photo?.image {
-                                        Image(uiImage: image)
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
+                        .opacity(model.photo?.image == nil ? 1.0 : 0)
+                        .overlay {
+                            if let image = model.photo?.image {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
 //                                            .animation(.spring())
-                                            .toolbar {
-                                                ToolbarItem(placement: .primaryAction) {
-                                                    Button("Use photo") {
-                                                        guard let uiImage = model.photo?.image else { return }
-                                                        onUse(uiImage)
-                                                        dismiss()
-                                                    }
-                                                }
+                                    .toolbar {
+                                        ToolbarItem(placement: .primaryAction) {
+                                            Button("Use photo") {
+                                                guard let uiImage = model.photo?.image else { return }
+                                                onUse(uiImage)
+                                                dismiss()
                                             }
-                                            .animation(.spring(), value: image)
+                                        }
                                     }
-                                }
-                            
-                            
-                            ZStack {
-                                captureButton
-                                    .offset(y: -10)
-                                HStack {
-                                    if model.photo == nil {
-                                        Button(action: {
-                                            model.switchFlash()
-                                        }, label: {
-                                            Image(systemName: model.isFlashOn ? "bolt.fill" : "bolt.slash.fill")
-                                                .font(.system(size: 20, weight: .medium, design: .default))
-                                                .padding(10)
-                                                .contentShape(Rectangle())
-                                        })
-                                        .accentColor(model.isFlashOn ? .yellow : .white)
-                                    }
-                                    else {
-                                        Button(action: {
-                                            withAnimation {
-                                                model.photo = nil
-                                            }
-                                        }, label: {
-                                            Text("Retake")
-                                                .foregroundColor(.white)
-                                                .padding(10)
-                                                .padding(10)
-                                                .contentShape(Rectangle())
-                                        })
-                                    }
-                                    
-                                    Spacer()
-                                    
-                                    flipCameraButton
-                                }
-                                .padding(.horizontal, 20)
+                                    .animation(.spring(), value: image)
                             }
                         }
-                    }
-                }
-                .onAppear {
-                    model.configure()
-                }
-                .toolbar {
-                    ToolbarItem(placement: .cancellationAction) {
-                        Button("Cancel") {
-                            dismiss()
+                    
+                    
+                    ZStack {
+                        captureButton
+                            .offset(y: -10)
+                        HStack {
+                            if model.photo == nil {
+                                Button(action: {
+                                    model.switchFlash()
+                                }, label: {
+                                    Image(systemName: model.isFlashOn ? "bolt.fill" : "bolt.slash.fill")
+                                        .font(.system(size: 20, weight: .medium, design: .default))
+                                        .padding(10)
+                                        .contentShape(Rectangle())
+                                })
+                                .accentColor(model.isFlashOn ? .yellow : .white)
+                            }
+                            else {
+                                Button(action: {
+                                    withAnimation {
+                                        model.photo = nil
+                                    }
+                                }, label: {
+                                    Text("Retake")
+                                        .foregroundColor(.white)
+                                        .padding(10)
+                                        .padding(10)
+                                        .contentShape(Rectangle())
+                                })
+                            }
+                            
+                            Spacer()
+                            
+                            flipCameraButton
                         }
+                        .padding(.horizontal, 20)
                     }
                 }
+            }
         }
-        .nbUseNavigationStack(.never)
+        .onAppear {
+            model.configure()
+        }
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+        }
     }
 }
 
