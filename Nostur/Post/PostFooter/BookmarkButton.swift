1392ffbde536044dee0b886b99d5a4251c5359f6
Fabian <fabian@nostur.com>
2024-03-23 12:09:14 +0100

    Add bookmark colors: add color selector on long tap

--- Nostur/Post/PostFooter/BookmarkButton.swift
+++ Nostur/Post/PostFooter/BookmarkButton.swift
@@ -13,6 +13,7 @@ struct BookmarkButton: View {
     private var isFirst: Bool
     private var isLast: Bool
     private var theme: Theme
+    @State private var showColorSelector = false
     
     init(nrPost: NRPost, isFirst: Bool = false, isLast: Bool = false, theme: Theme) {
         self.nrPost = nrPost
@@ -31,15 +32,96 @@ struct BookmarkButton: View {
             .foregroundColor(footerAttributes.bookmarked ? footerAttributes.bookmarkColor : theme.footerButtons)
             .contentShape(Rectangle())
             .padding(.trailing, isLast ? -10 : 0)
+            .simultaneousGesture(
+                LongPressGesture()
+                    .onEnded { _ in
+                        self.longTap()
+                    }
+            )
             .highPriorityGesture(
                 TapGesture()
                     .onEnded { _ in
                         tap()
                     }
             )
+            .overlay { // "orange", "red", "blue", "purple", "green"
+                if showColorSelector {
+                    HStack(spacing: 0) {
+                        Image(systemName: "bookmark.fill")
+                            .foregroundColor(.red)
+                            .contentShape(Rectangle())
+                            .padding(.leading, 10)
+                            .padding(.trailing, 10)
+                            .padding(.vertical, 10)
+                            .onTapGesture {
+                                self.addBookmark(.red)
+                                showColorSelector = false
+                            }
+                        
+                        Image(systemName: "bookmark.fill")
+                            .foregroundColor(.blue)
+                            .contentShape(Rectangle())
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 10)
+                            .onTapGesture {
+                                self.addBookmark(.blue)
+                                showColorSelector = false
+                            }
+                        
+                        Image(systemName: "bookmark.fill")
+                            .foregroundColor(.purple)
+                            .contentShape(Rectangle())
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 10)
+                            .onTapGesture {
+                                self.addBookmark(.purple)
+                                showColorSelector = false
+                            }
+                        
+                        Image(systemName: "bookmark.fill")
+                            .foregroundColor(.green)
+                            .contentShape(Rectangle())
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 10)
+                            .onTapGesture {
+                                self.addBookmark(.green)
+                                showColorSelector = false
+                            }
+                        
+                        Image(systemName: "bookmark.fill")
+                            .foregroundColor(.orange)
+                            .contentShape(Rectangle())
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 10)
+                            .onTapGesture {
+                                self.addBookmark(.orange)
+                                showColorSelector = false
+                            }
+                        
+                        Image(systemName: "bookmark")
+                            .foregroundColor(.orange)
+                            .contentShape(Rectangle())
+                            .padding(.leading, 10)
+                            .padding(.trailing, 10)
+                            .padding(.vertical, 10)
+                            .onTapGesture {
+                                self.removeBookmark()
+                                showColorSelector = false
+                            }
+                    }
+                        
+                    .background(theme.listBackground)
+                    .zIndex(1)
+                    .offset(x: -95)
+//                        .frame(width: 100, height: 100)
+                    
+                }
+            }
+            .zIndex(1)
     }
     
     private func tap() {
+        guard !showColorSelector else { return }
         if footerAttributes.bookmarked {
             self.removeBookmark()
         }
@@ -72,5 +154,43 @@ struct BookmarkButton: View {
             accountCache()?.removeBookmark(nrPost.id)
         }
     }
+    
+    private func longTap() {
+        showColorSelector = true
+    }
 }
 
+
+
+#Preview("Bookmark button") {
+    
+    PreviewContainer({ pe in
+        pe.loadContacts()
+        pe.loadPosts()
+    }) {
+        HStack(spacing: 0) {
+            Circle()
+                .foregroundColor(Color.random)
+                .frame(width: DIMENSIONS.POST_ROW_PFP_DIAMETER)
+                .frame(width: DIMENSIONS.POST_ROW_PFP_DIAMETER + 10.0 + 10.0, alignment: .top)
+                .fixedSize()
+            VStack(spacing: 10) {
+                if let p = PreviewFetcher.fetchNRPost("21a1b8e4083c11eab8f280dc0c0bddf3837949df75662e181ad117bd0bd5fdf3") {
+                    BookmarkButton(nrPost: p, isLast: true, theme: Themes.default.theme)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                else {
+                    Text("Nothing")
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+            }
+            .padding(.trailing, 10)
+        }
+        .padding(.top, 100)
+        .padding(.vertical, 10)
+        .background(.gray.opacity(0.1))
+        .frame(width: .infinity, height: 150)
+        .clipped()
+//        .fixedSize()
+    }
+}
