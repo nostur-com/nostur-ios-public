53f5dc23a0df08dbd5c5dbb94b37538371ac854d
Fabian <fabian@nostur.com>
2024-04-04 16:07:44 +0200

    Fix detail post not scrolling into view
    - .parentPosts.count > 0 is not a good check because .parentPosts are not always loaded (ie in search results)
    - check .replyToId instead

--- Nostur/Post/PostDetailView.swift
+++ Nostur/Post/PostDetailView.swift
@@ -606,7 +606,7 @@ struct DetailPost: View {
                 }
             }
             .onAppear {
-                guard nrPost.parentPosts.count > 0 else { return } // don't scroll if we already the root
+                guard nrPost.replyToId != nil else { return } // don't scroll if we already the root
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
                     sendNotification(.scrollToDetail, nrPost.id)
                 }
