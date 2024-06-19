7b4078a57f7be2b34848581bdb63cc343c7c3b7a
Fabian <fabian@nostur.com>
2024-06-19 13:08:11 +0200

    WIP account switcher

--- Nostur/Post/PostComposer/PostAccountSwitcher.swift
+++ Nostur/Post/PostComposer/PostAccountSwitcher.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// TODO: .fanOutDirection doesn't work well yet so only use .bottom for now
 struct InlineAccountSwitcher: View, Equatable {
     static func == (lhs: InlineAccountSwitcher, rhs: InlineAccountSwitcher) -> Bool {
         lhs.activeAccount == rhs.activeAccount //&& lhs.accounts.count == rhs.accounts.count
@@ -15,6 +16,7 @@ struct InlineAccountSwitcher: View, Equatable {
     public var activeAccount: CloudAccount
     public var onChange: (CloudAccount) -> ()
     public var size: CGFloat = 50.0
+    public var fanOutDirection: Direction = .bottom
     
     @State private var expanded = false
     
@@ -30,6 +32,79 @@ struct InlineAccountSwitcher: View, Equatable {
             })
     }
     
+    @ViewBuilder
+    private var bottom: some View {
+        VStack(spacing: 2) {
+            ForEach(accountsSorted.indices, id:\.self) { index in
+                PFP(pubkey: accountsSorted[index].publicKey, account: accountsSorted[index], size: size)
+                    .onTapGesture {
+                        accountTapped(accountsSorted[index])
+                    }
+                    .opacity(index == 0 || expanded ? 1.0 : 0.2)
+                    .zIndex(-Double(index))
+                    .offset(y: expanded || (index == 0) ? 0 : (Double(index) * -(size - 2)))
+                    .animation(.easeOut(duration: 0.2), value: expanded)
+                    .id(accountsSorted[index].publicKey) // sorting index and view identity (publicKey) is different!
+            }
+        }
+        .fixedSize()
+    }
+    
+    @ViewBuilder
+    private var top: some View {
+        VStack(spacing: 2) {
+            ForEach(accountsSorted.indices, id:\.self) { index in
+                PFP(pubkey: accountsSorted[index].publicKey, account: accountsSorted[index], size: size)
+                    .onTapGesture {
+                        accountTapped(accountsSorted[index])
+                    }
+                    .opacity(index == 0 || expanded ? 1.0 : 0.2)
+                    .zIndex(-Double(index))
+                    .offset(y: (expanded || (index == 0)) ? (Double(index) * -(size*2)) : (Double(index) * -(size + 4)))
+                    .animation(.easeOut(duration: 0.2), value: expanded)
+                    .id(accountsSorted[index].publicKey) // sorting index and view identity (publicKey) is different!
+            }
+        }
+        .fixedSize()
+    }
+    
+//    @ViewBuilder
+//    private var leading: some View {
+//        VStack(spacing: 2) {
+//            ForEach(accountsSorted.indices, id:\.self) { index in
+//                PFP(pubkey: accountsSorted[index].publicKey, account: accountsSorted[index], size: size)
+//                    .onTapGesture {
+//                        accountTapped(accountsSorted[index])
+//                    }
+//                    .opacity(index == 0 || expanded ? 1.0 : 0.2)
+//                    .zIndex(-Double(index))
+//                    .offset(y: (Double(index) * -(size - 0)))
+//                    .offset(x: expanded || (index == 0) ? 0 : (Double(index) * -(size - 2)))
+//                    .animation(.easeOut(duration: 0.2), value: expanded)
+//                    .id(accountsSorted[index].publicKey) // sorting index and view identity (publicKey) is different!
+//            }
+//        }
+//        .fixedSize()
+//    }
+//    
+//    @ViewBuilder
+//    private var trailing: some View {
+//        VStack(spacing: 2) {
+//            ForEach(accountsSorted.indices, id:\.self) { index in
+//                PFP(pubkey: accountsSorted[index].publicKey, account: accountsSorted[index], size: size)
+//                    .onTapGesture {
+//                        accountTapped(accountsSorted[index])
+//                    }
+//                    .opacity(index == 0 || expanded ? 1.0 : 0.2)
+//                    .zIndex(-Double(index))
+//                    .offset(x: (expanded || (index == 0)) ? (Double(index) * -(size*2)) : (Double(index) * -(size + 4)))
+//                    .animation(.easeOut(duration: 0.2), value: expanded)
+//                    .id(accountsSorted[index].publicKey) // sorting index and view identity (publicKey) is different!
+//            }
+//        }
+//        .fixedSize()
+//    }
+    
     var body: some View {
         #if DEBUG
         let _ = Self._printChanges()
@@ -37,20 +112,16 @@ struct InlineAccountSwitcher: View, Equatable {
         Color.clear
             .frame(width: size, height: size)
             .overlay(alignment: .topLeading) {
-                VStack(spacing: 2) {
-                    ForEach(accountsSorted.indices, id:\.self) { index in
-                        PFP(pubkey: accountsSorted[index].publicKey, account: accountsSorted[index], size: size)
-                            .onTapGesture {
-                                accountTapped(accountsSorted[index])
-                            }
-                            .opacity(index == 0 || expanded ? 1.0 : 0.2)
-                            .zIndex(-Double(index))
-                            .offset(y: expanded || (index == 0) ? 0 : (Double(index) * -(size - 2)))
-                            .animation(.easeOut(duration: 0.2), value: expanded)
-                            .id(accountsSorted[index].publicKey) // sorting index and view identity (publicKey) is different!
-                    }
+                switch fanOutDirection {
+                case .leading:
+                    self.bottom
+                case .trailing:
+                    self.bottom
+                case .top:
+                    self.bottom
+                case .bottom:
+                    self.bottom
                 }
-                .fixedSize()
             }
             .task {
                 accounts = NRState.shared.accounts
@@ -76,13 +147,21 @@ struct InlineAccountSwitcher: View, Equatable {
     }
 }
 
+enum Direction {
+    case leading
+    case trailing
+    case top
+    case bottom
+}
+
 struct InlineAccountSwitcherPreviewWrap: View {
+    public var fanOutDirection: Direction = .bottom
     @State var activeAccount = NRState.shared.loggedInAccount!.account
     
     var body: some View {
         InlineAccountSwitcher(activeAccount: activeAccount, onChange: { account in
             activeAccount = account
-        }, size: 20.0)
+        }, size: 20.0, fanOutDirection: fanOutDirection)
     }
 }
 
@@ -90,7 +169,20 @@ struct InlineAccountSwitcher_Previews: PreviewProvider {
 
     static var previews: some View {
         PreviewContainer({ pe in pe.loadAccounts() }) {
-            InlineAccountSwitcherPreviewWrap()
+            HStack {
+                VStack {
+                    Text("Test")
+                    InlineAccountSwitcherPreviewWrap(fanOutDirection: .top)
+                    Text("End test")
+                    InlineAccountSwitcherPreviewWrap(fanOutDirection: .trailing)
+                }
+                VStack {
+                    InlineAccountSwitcherPreviewWrap(fanOutDirection: .leading)
+                    Text("Test")
+                    InlineAccountSwitcherPreviewWrap(fanOutDirection: .bottom)
+                    Text("End test")
+                }
+            }
         }
     }
 }
