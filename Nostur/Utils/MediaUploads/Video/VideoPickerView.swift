584cc4cb0f8b531c99016b33a5cc0fed9bcf329b
Fabian <fabian@nostur.com>
2024-06-21 02:15:29 +0200

    Clean up video uploading WIP
    - make sure metadata is stripped during compression
    - remove test code snippets
    - add synchronous version of compressVideo()

--- Nostur/Utils/MediaUploads/Video/VideoPickerView.swift
+++ Nostur/Utils/MediaUploads/Video/VideoPickerView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import UIKit
 import MobileCoreServices
+import AVFoundation
 
 struct VideoPickerView: UIViewControllerRepresentable {
     @Binding var selectedVideoURL: URL?
@@ -44,121 +45,29 @@ struct VideoPickerView: UIViewControllerRepresentable {
 }
 
 // Compress video
-
-import AVFoundation
-
 func compressVideo(inputURL: URL, outputURL: URL, handler:@escaping (AVAssetExportSession.Status) -> Void) {
     let asset = AVAsset(url: inputURL)
     let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetMediumQuality)!
     
     exportSession.outputURL = outputURL
     exportSession.outputFileType = .mp4
+    exportSession.metadata = []
     exportSession.exportAsynchronously {
         handler(exportSession.status)
     }
 }
 
-// Upload video
-
-func uploadVideo(url: URL, completion: @escaping (Result<Bool, Error>) -> Void) {
-    var request = URLRequest(url: URL(string: "http://localhost:8080/wp-json/nostrmedia/v1/upload/")!)
-    request.httpMethod = "POST"
-
-    let boundary = UUID().uuidString
-    request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
-
-    let data = createBody(with: ["file": url], boundary: boundary)
-    let task = URLSession.shared.uploadTask(with: request, from: data) { data, response, error in
-        defer {
-            // Cleanup the temporary file
-            try? FileManager.default.removeItem(at: url)
-        }
-        
-        if let error = error {
-            completion(.failure(error))
-            return
-        }
-
-        completion(.success(true))
-    }
-    task.resume()
-}
-
-
-func createBody(with parameters: [String: URL], boundary: String) -> Data {
-    var body = Data()
-
-    for (key, url) in parameters {
-        let filename = url.lastPathComponent
-        let data = try! Data(contentsOf: url)
-        let mimetype = "video/mp4"
-
-        body.append("--\(boundary)\r\n".data(using: .utf8)!)
-        body.append("Content-Disposition: form-data; name=\"\(key)\"; filename=\"\(filename)\"\r\n".data(using: .utf8)!)
-        body.append("Content-Type: \(mimetype)\r\n\r\n".data(using: .utf8)!)
-        body.append(data)
-        body.append("\r\n".data(using: .utf8)!)
-    }
-
-    body.append("--\(boundary)--\r\n".data(using: .utf8)!)
-    return body
-}
-
-//
-
-struct VideoButton: View {
-    @State private var selectedVideoURL: URL?
-    @State private var isShowingVideoPicker = false
-    @State private var isUploading = false
-    @State private var uploadStatus: String = ""
+func compressVideoSynchronously(inputURL: URL, outputURL: URL) -> URL? {
+    let semaphore = DispatchSemaphore(value: 0)
+    var resultURL: URL? = nil
     
-    var body: some View {
-        VStack {
-            if let selectedVideoURL = selectedVideoURL {
-                Text("Selected Video: \(selectedVideoURL.lastPathComponent)")
-                
-                Button("Compress and Upload Video") {
-                    let compressedURL = URL(fileURLWithPath: NSTemporaryDirectory() + UUID().uuidString + ".mp4")
-                    isUploading = true
-                    
-                    compressVideo(inputURL: selectedVideoURL, outputURL: compressedURL) { status in
-                        if status == .completed {
-                            uploadVideo(url: compressedURL) { result in
-                                isUploading = false
-                                switch result {
-                                case .success:
-                                    uploadStatus = "Upload Successful!"
-                                case .failure(let error):
-                                    uploadStatus = "Upload Failed: \(error.localizedDescription)"
-                                }
-                                // Cleanup the selected video URL as well
-                                try? FileManager.default.removeItem(at: selectedVideoURL)
-                                self.selectedVideoURL = nil
-                            }
-                        } else {
-                            isUploading = false
-                            uploadStatus = "Compression Failed"
-                        }
-                    }
-                }
-                .padding()
-                
-                Text(uploadStatus)
-                    .padding()
-            } else {
-                Button("Select Video") {
-                    isShowingVideoPicker = true
-                }
-                .padding()
-            }
+    compressVideo(inputURL: inputURL, outputURL: outputURL) { status in
+        if status == .completed {
+            resultURL = outputURL
         }
-        .sheet(isPresented: $isShowingVideoPicker) {
-            VideoPickerView(selectedVideoURL: $selectedVideoURL)
-        }
-        .overlay(
-            isUploading ? ProgressView("Uploading...").padding().background(Color.white).cornerRadius(10) : nil
-        )
+        semaphore.signal()
     }
+    
+    semaphore.wait()
+    return resultURL
 }
-
-
