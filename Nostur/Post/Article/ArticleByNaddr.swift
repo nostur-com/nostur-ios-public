d9871cb402fefbca2e006b99e063c9225440cd0b
Fabian <fabian@nostur.com>
2024-06-29 15:42:47 +0200

    Refactor ArticleByNaddr
    - use same FetchVM method as NEventView

--- Nostur/Post/Article/ArticleByNaddr.swift
+++ Nostur/Post/Article/ArticleByNaddr.swift
@@ -8,90 +8,111 @@
 import SwiftUI
 
 struct ArticleByNaddr: View {
-    public let naddr1:String
-    public var navigationTitle:String? = nil
+    @EnvironmentObject private var settings: SettingsStore
+    @EnvironmentObject private var dim: DIMENSIONS
+    public let naddr1: String
+    public var navigationTitle: String? = nil
     public var navTitleHidden: Bool = false
-    public var theme:Theme = Themes.default.theme
-    @State var article:NRPost? = nil
-    @State var backlog = Backlog(timeout: 15, auto: true)
-    @State var error:String? = nil
+    public var theme: Theme = Themes.default.theme
+    @StateObject private var vm = FetchVM<NRPost>(timeout: 5.0, debounceTime: 0.05)
     
     var body: some View {
         VStack {
-            if let error  {
-                Text(error)
-            }
-            else if let article {
-                if article.kind == 30023 {
-                    ArticleView(article, isDetail: true, navTitleHidden: navTitleHidden, theme: theme)
+            switch vm.state {
+            case .initializing, .loading, .altLoading:
+                HStack(spacing: 5) {
+                    ProgressView()
+                    if vm.state == .initializing || vm.state == .loading {
+                        Text("Fetching...")
+                    }
+                    else if vm.state == .altLoading {
+                        Text("Trying more relays...")
+                    }
                 }
-                else {
-                    PostDetailView(nrPost: article, navTitleHidden: navTitleHidden)
-                }
-            }
-            else {
-                ProgressView()
-                    .onAppear { [weak backlog] in
-                        bg().perform {
-                            if let naddr = try? ShareableIdentifier(naddr1),
-                               let kind = naddr.kind,
-                               let pubkey = naddr.pubkey,
-                               let definition = naddr.eventId
-                            {
-                                if let article = Event.fetchReplacableEvent(kind,
-                                                                                 pubkey: pubkey,
-                                                                                 definition: definition,
-                                                                                 context: bg()) {
-                                    let article = NRPost(event: article)
-                                    DispatchQueue.main.async {
-                                        self.article = article
+                    .padding(10)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .onAppear { [weak vm, weak dim] in
+                        guard let naddr = try? ShareableIdentifier(naddr1),
+                              let kind = naddr.kind,
+                              let pubkey = naddr.pubkey,
+                              let definition = naddr.eventId
+                        else {
+                            return
+                        }
+                        
+                        
+                        let fetchParams: FetchVM.FetchParams = (
+                            prio: true,
+                            req: { [weak vm, weak dim] taskId in
+                                bg().perform { [weak vm, weak dim] in // 1. CHECK LOCAL DB
+                                    guard let vm, let dim else { return }
+                                    if let event = Event.fetchReplacableEvent(kind,
+                                                                                   pubkey: pubkey,
+                                                                                   definition: definition,
+                                                                                   context: bg()) {
+                                        vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
+                                    }
+                                    else { // 2. ELSE CHECK RELAY
+                                        req(RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId))
+                              
                                     }
                                 }
-                                else {
-                                    let reqTask = ReqTask(
-                                        prio: true,
-                                        prefix: "ARTICLE-",
-                                        reqCommand: { taskId in
-                                            req(RM.getArticle(pubkey: pubkey, kind:Int(kind), definition:definition, subscriptionId: taskId))
-                                        },
-                                        processResponseCommand: { taskId, _, article in
-                                            bg().perform {
-                                                guard let backlog else { return }
-                                                if let article = article {
-                                                    let article = NRPost(event: article)
-                                                    DispatchQueue.main.async {
-                                                        self.article = article
-                                                    }
-                                                    backlog.clear()
-                                                }
-                                                else if let article = Event.fetchReplacableEvent(kind,
-                                                                                                 pubkey: pubkey,
-                                                                                                 definition: definition,
-                                                                                                 context: bg()) {
-                                                    let article = NRPost(event: article)
-                                                    DispatchQueue.main.async {
-                                                        self.article = article
-                                                    }
-                                                    backlog.clear()
-                                                }
-                                            }
-                                        },
-                                        timeoutCommand: { taskId in
-                                            DispatchQueue.main.async {
-                                                self.error = "Could not find article"
-                                            }
-                                        })
-                                    
-                                    guard let backlog else { return }
-                                    backlog.add(reqTask)
-                                    reqTask.fetch()
+                            },
+                            onComplete: { [weak vm, weak dim] relayMessage, event in
+                                guard let vm, let dim else { return }
+                                if let event = event {
+                                    vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
                                 }
+                                else if let event = Event.fetchReplacableEvent(kind,
+                                                                                    pubkey: pubkey,
+                                                                                    definition: definition,
+                                                                                    context: bg()) { // 3. WE FOUND IT ON RELAY
+                                    if vm.state == .altLoading, let relay = naddr.relays.first {
+                                        L.og.debug("Event found on using relay hint: \(event.id) - \(relay)")
+                                    }
+                                    vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
+                                }
+                                // Still don't have the event? try to fetch from relay hint
+                                // TODO: Should try a relay we don't already have in our relay set
+                                else if settings.followRelayHints && [.initializing, .loading].contains(vm.state) {
+                                    // try search relays and relay hint
+                                    vm.altFetch()
+                                }
+                                else { // 5. TIMEOUT
+                                    vm.timeout()
+                                }
+                            },
+                            altReq: { taskId in // IF WE HAVE A RELAY HINT WE USE THIS REQ, TRIGGERED BY vm.altFetch()
+                                // Try search relays
+                                req(RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId), relayType: .SEARCH)
+                                guard let relay = naddr.relays.first else { return }
+                                
+                                L.og.debug("FetchVM.3 HINT \(relay)")
+                                ConnectionPool.shared.sendEphemeralMessage(
+                                    RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId),
+                                    relay: relay
+                                )
                             }
-                            else {
-                                L.og.error("Could not decode all details from naddr1: \(naddr1)")
-                            }
-                        }
+                            
+                        )
+                        vm?.setFetchParams(fetchParams)
+                        vm?.fetch()
                     }
+            case .ready(let nrPost):
+                if nrPost.kind == 30023 {
+                    ArticleView(nrPost, isDetail: true, navTitleHidden: navTitleHidden, theme: theme)
+                }
+                else {
+                    PostDetailView(nrPost: nrPost, navTitleHidden: navTitleHidden)
+                }
+            case .timeout:
+                Text("Unable to fetch content")
+                    .padding(10)
+                    .frame(maxWidth: .infinity, alignment: .center)
+            case .error(let error):
+                Text(error)
+                    .padding(10)
+                    .frame(maxWidth: .infinity, alignment: .center)
             }
         }
     }
