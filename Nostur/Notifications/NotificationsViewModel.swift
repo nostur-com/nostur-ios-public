6cdbda5a14b1805a1f88657fca185c094bdeb553
Fabian <fabian@nostur.com>
2024-08-17 00:00:22 +0200

    Fix wrong queue

--- Nostur/Notifications/NotificationsViewModel.swift
+++ Nostur/Notifications/NotificationsViewModel.swift
@@ -946,33 +946,39 @@ fileprivate class NotificationFetchRequests {
 
 
 class OfflinePosts {
+    
+    
     static func checkForOfflinePosts(_ maxAgo:TimeInterval = 3600 * 24 * 3) { // 3 days
-        guard ConnectionPool.shared.anyConnected else { return }
-        guard let account = account() else { return }
-        let pubkey = account.publicKey
-        let xDaysAgo = Date.now.addingTimeInterval(-(maxAgo))
-        
-        let r1 = Event.fetchRequest()
-        // X days ago, from our pubkey, only kinds that we can create+send
-        r1.predicate = NSPredicate(format:
-                                    "created_at > %i " +
-                                    "AND pubkey = %@ " +
-                                    "AND kind IN {0,1,3,4,5,6,7,9802,34235} " +
-                                    "AND relays = \"\"" +
-                                    "AND NOT flags IN {\"nsecbunker_unsigned\",\"awaiting_send\",\"draft\"}" +
-                                    "AND sig != nil",
-                                    Int64(xDaysAgo.timeIntervalSince1970),
-                                    pubkey)
-        r1.fetchLimit = 100 // sanity
-        r1.sortDescriptors = [NSSortDescriptor(keyPath:\Event.created_at, ascending: false)]
-        
-        if let offlinePosts = try? bg().fetch(r1) {
-            guard !offlinePosts.isEmpty else { return }
-            for offlinePost in offlinePosts {
-                L.og.debug("Publishing offline post: \(offlinePost.id)")
-                let nEvent = offlinePost.toNEvent()
-                DispatchQueue.main.async {
-                    Unpublisher.shared.publishNow(nEvent)
+        DispatchQueue.main.async {
+            guard ConnectionPool.shared.anyConnected else { return }
+            bg().perform {
+                guard let account = account() else { return }
+                let pubkey = account.publicKey
+                let xDaysAgo = Date.now.addingTimeInterval(-(maxAgo))
+                
+                let r1 = Event.fetchRequest()
+                // X days ago, from our pubkey, only kinds that we can create+send
+                r1.predicate = NSPredicate(format:
+                                            "created_at > %i " +
+                                            "AND pubkey = %@ " +
+                                            "AND kind IN {0,1,3,4,5,6,7,9802,34235} " +
+                                            "AND relays = \"\"" +
+                                            "AND NOT flags IN {\"nsecbunker_unsigned\",\"awaiting_send\",\"draft\"}" +
+                                            "AND sig != nil",
+                                            Int64(xDaysAgo.timeIntervalSince1970),
+                                            pubkey)
+                r1.fetchLimit = 100 // sanity
+                r1.sortDescriptors = [NSSortDescriptor(keyPath:\Event.created_at, ascending: false)]
+                
+                if let offlinePosts = try? bg().fetch(r1) {
+                    guard !offlinePosts.isEmpty else { return }
+                    for offlinePost in offlinePosts {
+                        L.og.debug("Publishing offline post: \(offlinePost.id)")
+                        let nEvent = offlinePost.toNEvent()
+                        DispatchQueue.main.async {
+                            Unpublisher.shared.publishNow(nEvent)
+                        }
+                    }
                 }
             }
         }
