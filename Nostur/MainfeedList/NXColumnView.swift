5f9394656224c9778ad6e770b2c3762be56cac0c
Fabian <fabian@nostur.com>
2024-09-16 23:56:04 +0200

    Fix viewModel.resume() triggered too soon

--- Nostur/MainfeedList/NXColumnView.swift
+++ Nostur/MainfeedList/NXColumnView.swift
@@ -76,17 +76,17 @@ struct NXColumnView: View {
         .onChange(of: scenePhase) { newScenePhase in
             switch newScenePhase {
             case .active:
-                if !IS_CATALYST {
+                if isVisible && !IS_CATALYST {
                     if (NRState.shared.appIsInBackground) { // if we were actually in background (from .background, not just a few seconds .inactive)
                         viewModel.resume()
                     }
                 }
-                else {
+                else if isVisible {
                     viewModel.resume()
                 }
                 
             case .background:
-                if !IS_CATALYST {
+                if !IS_CATALYST && ((!config.id.starts(with: "Following-") || config.name == "Explore")) {
                     viewModel.pause()
                 }
             default:
