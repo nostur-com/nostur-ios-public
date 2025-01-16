8178a39988a9074ed893610b9f3f7653ba9a51f0
Fabian <fabian@nostur.com>
2025-01-16 10:57:52 +0100

    Optimize/clean up queued fetcher
    - remove unnecessary PassthroughSubject steps

--- Nostur/Post/NR/QueuedFetcher.swift
+++ Nostur/Post/NR/QueuedFetcher.swift
@@ -10,49 +10,24 @@ import Combine
 
 class QueuedFetcher {
     static let shared = QueuedFetcher()
-    
-    var subscriptions = Set<AnyCancellable>()
-    
-    var pQueue = Set<String>()
-    var idQueue = Set<String>()
+
+    private var pQueue = Set<String>()
+    private var idQueue = Set<String>()
     
     // TODO: finish recent cache - add recently tried queue, to not retry the same over and over..
-    var recentPs = Set<String>()
-    var recentIds = Set<String>()
-    
+    private var recentPs = Set<String>()
+    private var recentIds = Set<String>()
+
+    private var fetchSubscription: AnyCancellable?
     private var fetchSubject = PassthroughSubject<Void, Never>()
-    private var enqueuePsubject = PassthroughSubject<String, Never>()
-    private var dequeuePsubject = PassthroughSubject<String, Never>()
-    private var enqueueIDsubject = PassthroughSubject<String, Never>()
-    private var dequeueIDsubject = PassthroughSubject<String, Never>()
     private var ctx = bg()
     
     
     init() {
-        enqueuePsubject.sink { [weak self] pTag in
-            self?.pQueue.insert(pTag)
-        }
-        .store(in: &subscriptions)
-        
-        dequeuePsubject.sink { [weak self] pTag in
-            self?.pQueue.remove(pTag)
-        }
-        .store(in: &subscriptions)
-        
-        enqueueIDsubject.sink { [weak self] eventId in
-            self?.idQueue.insert(eventId)
-        }
-        .store(in: &subscriptions)
-        
-        dequeueIDsubject.sink { [weak self] eventId in
-            self?.idQueue.remove(eventId)
-        }
-        .store(in: &subscriptions)
-        
-        fetchSubject
+        fetchSubscription = fetchSubject
             .debounce(for: .seconds(0.05), scheduler: RunLoop.main)
             .sink { [weak self] _ in
-                self?.ctx.perform {
+                self?.ctx.perform { [weak self] in
                     guard let self else { return }
                     guard !self.pQueue.isEmpty || !self.idQueue.isEmpty else { return }
                     
@@ -73,90 +48,75 @@ class QueuedFetcher {
                     }
                 }
         }
-        .store(in: &subscriptions)
     }
     
     public func enqueue(pTag: String) {
         ctx.perform { [weak self] in
-            self?.enqueuePsubject.send(pTag)
-            self?.fetch()
+            self?.pQueue.insert(pTag)
+            self?.fetchSubject.send()
         }
     }
     
     public func enqueue(pTags: [String]) {
+        guard !pTags.isEmpty else { return }
         ctx.perform { [weak self] in
-            guard !pTags.isEmpty else { return }
-            pTags.forEach { pTag in
-                self?.enqueuePsubject.send(pTag)
-            }
-            self?.fetch()
+            self?.pQueue.formUnion(pTags)
+            self?.fetchSubject.send()
         }
     }
     
     public func enqueue(pTags: Set<String>) {
         guard !pTags.isEmpty else { return }
         ctx.perform { [weak self] in
-            pTags.forEach { pTag in
-                self?.enqueuePsubject.send(pTag)
-            }
-            self?.fetch()
+            self?.pQueue.formUnion(pTags)
+            self?.fetchSubject.send()
         }
     }
     
     public func dequeue(pTag: String) {
         ctx.perform { [weak self] in
-            self?.dequeuePsubject.send(pTag)
+            self?.pQueue.remove(pTag)
         }
     }
     
     public func dequeue(pTags: [String]) {
+        guard !pTags.isEmpty else { return }
         ctx.perform { [weak self] in
-            pTags.forEach { pTag in
-                self?.dequeuePsubject.send(pTag)
-            }
+            self?.pQueue.subtract(Set(pTags))
         }
     }
     
     public func dequeue(pTags: Set<String>) {
+        guard !pTags.isEmpty else { return }
         ctx.perform { [weak self] in
-            pTags.forEach { pTag in
-                self?.dequeuePsubject.send(pTag)
-            }
+            self?.pQueue.subtract(pTags)
         }
     }
     
     public func enqueue(id: String) {
         ctx.perform { [weak self] in
-            self?.enqueueIDsubject.send(id)
-            self?.fetch()
+            self?.idQueue.insert(id)
+            self?.fetchSubject.send()
         }
     }
     
     public func enqueue(ids: [String]) {
         guard !ids.isEmpty else { return }
         ctx.perform { [weak self] in
-            ids.forEach { id in
-                self?.enqueuePsubject.send(id)
-            }
-            self?.fetch()
+            self?.idQueue.formUnion(Set(ids))
+            self?.fetchSubject.send()
         }
     }
     
     public func dequeue(id: String) {
         ctx.perform { [weak self] in
-            self?.dequeueIDsubject.send(id)
+            self?.idQueue.remove(id)
         }
     }
     
     public func dequeue(ids: [String]) {
         ctx.perform { [weak self] in
-            ids.forEach { id in
-                self?.dequeueIDsubject.send(id)
-            }
+            self?.idQueue.subtract(Set(ids))
         }
-    }
-    
-    private func fetch() {
-        fetchSubject.send()
     }
 }
