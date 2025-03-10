c9191cb878c7f9d7b45dbad0dd3b7532be8bc7da
Fabian <fabian@nostur.com>
2025-03-10 21:49:53 +0100

    Clean up

---
+++ Nostur/Post/ContentRendererParts/MediaView.swift
@@ -0,0 +1,18 @@
+//
+//  MediaView.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 10/03/2025.
+//
+
+import SwiftUI
+
+struct MediaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MediaView()
+}
