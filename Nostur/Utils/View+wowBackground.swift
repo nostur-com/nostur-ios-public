7b72f294ee9ad5368da6f44938f373d2ed59d970
Fabian <fabian@nostur.com>
2025-03-21 14:07:48 +0100

    Add nice background

---
+++ Nostur/Utils/View+wowBackground.swift
@@ -0,0 +1,28 @@
+//
+//  View+wowBackground.swift
+//  Nostur
+//
+//  Created by Fabian Lachman on 20/03/2025.
+//
+
+import SwiftUI
+
+extension View {
+    public func wowBackground() -> some View {
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color(red: 0.17, green: 0.96, blue: 0.92),
+                    Color(red: 0.15, green: 0.48, blue: 0.25)
+                    ]),
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .ignoresSafeArea()
+            
+            self
+                .foregroundColor(.white)
+        }
+    }
+}
