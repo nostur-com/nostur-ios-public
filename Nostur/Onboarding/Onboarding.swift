2407921aca4925e9f0510212384f506e1a68ac16
Fabian <fabian@nostur.com>
2025-03-23 18:10:21 +0100

    Fix Onboarding theme + networkmonitor environmentObject

--- Nostur/Onboarding/Onboarding.swift
+++ Nostur/Onboarding/Onboarding.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import NavigationBackport
 
 struct Onboarding: View {
-    @EnvironmentObject private var networkMonitor: NetworkMonitor
+    @ObservedObject private var networkMonitor: NetworkMonitor = .shared
     
     var body: some View {
         NBNavigationStack {
@@ -20,8 +20,11 @@ struct Onboarding: View {
                 .fullScreenCover(isPresented: $networkMonitor.isDisconnected) {
                     NoInternetView()
                 }
-                
         }
+        
+        // Sets toolbar (Back button) color
+        .tint(Themes.default.theme.accent)
+        .accentColor(Themes.default.theme.accent)
     }
 }
 
