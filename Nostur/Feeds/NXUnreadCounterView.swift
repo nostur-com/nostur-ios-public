ada303b67262d6b44e1d938e99175a9b6f872264
Fabian <fabian@nostur.com>
2025-04-03 12:53:35 +0200

    Adjust unread counter style

--- Nostur/Feeds/NXUnreadCounterView.swift
+++ Nostur/Feeds/NXUnreadCounterView.swift
@@ -15,27 +15,65 @@ struct NXUnreadCounterView: View {
     var body: some View {
         RoundedRectangle(cornerRadius: 20)
             .foregroundColor(themes.theme.accent)
-            .frame(width: 65, height: 40)
+            .frame(width: 61, height: 36)
             .overlay(alignment: .leading) {
                 Text(vm.unreadCount.description)
                     .animation(.snappy, value: vm.unreadCount)
                     .rollingNumber()
                     .fixedSize()
                     .frame(width: 35, alignment: .center)
-                    .padding(.leading, 5)
+                    .padding(.leading, 7)
                     
             }
             .overlay(alignment: .trailing) {
                 Image(systemName: "arrow.up")
-                    .padding(.trailing, 8)
+                    .padding(.trailing, 6)
+                    .font(.footnote)
             }
             .fontWeightBold()
             .foregroundColor(.white)
-            .opacity(vm.unreadCount > 0 ? 1.0 : 0)
+            .opacity(vm.unreadCount > 0 ? 0.9 : 0)
+            .padding(5)
+            .contentShape(Rectangle())
     }
 }
 
+@available(iOS 17.0, *)
 #Preview {
-    NXUnreadCounterView(vm: NXColumnViewModelInner())
-        .environmentObject(Themes.default)
+    @Previewable @State var vmInner = NXColumnViewModelInner()
+    @Previewable @State var vmInner2 = NXColumnViewModelInner()
+    @Previewable @State var vmInner3 = NXColumnViewModelInner()
+    @Previewable @State var vmInner4 = NXColumnViewModelInner()
+    ZStack {
+        
+        let _ = Themes.default.loadDefault()
+        
+        Themes.default.theme.background
+        
+        VStack(spacing: 20) {
+            NXUnreadCounterView(vm: vmInner)
+                .environmentObject(Themes.default)
+                .onAppear {
+                    vmInner.unreadIds["test"] = 5
+                }
+            
+            NXUnreadCounterView(vm: vmInner2)
+                .environmentObject(Themes.default)
+                .onAppear {
+                    vmInner2.unreadIds["test"] = 27
+                }
+            
+            NXUnreadCounterView(vm: vmInner3)
+                .environmentObject(Themes.default)
+                .onAppear {
+                    vmInner3.unreadIds["test"] = 342
+                }
+            
+            NXUnreadCounterView(vm: vmInner4)
+                .environmentObject(Themes.default)
+                .onAppear {
+                    vmInner4.unreadIds["test"] = 3420
+                }
+        }
+    }
 }
