2abbc0f9d9e180e4da3cf36064cf6b2eddc80ea1
Fabian <fabian@nostur.com>
2025-05-17 00:37:26 +0200

    Optimize count (2)
    replace .filter() and .count with .count(where:)

--- Nostur/Relays/Network/ConnectionPool.swift
+++ Nostur/Relays/Network/ConnectionPool.swift
@@ -80,11 +80,11 @@ public class ConnectionPool: ObservableObject {
     }
     
     public var connectedCount: Int {
-        connections.count({ $0.value.isConnected })
+        connections.count(where: { $0.value.isConnected })
     }
     
     public var ephemeralConnectedCount: Int {
-        ephemeralConnections.count({ $0.value.isConnected })
+        ephemeralConnections.count(where: { $0.value.isConnected })
     }
     
     private var stayConnectedTimer: Timer?
