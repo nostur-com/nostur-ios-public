62a2a32a10769123641cf51c5a291882a1ebeb1b
Fabian <fabian@nostur.com>
2025-05-27 20:54:23 +0200

    Remove wrapper
    probably not needed anymore

--- Nostur/Post/ContentRendererParts/NEventView.swift
+++ Nostur/Post/ContentRendererParts/NEventView.swift
@@ -19,104 +19,102 @@ struct NEventView: View {
     @StateObject private var vm = FetchVM<NRPost>(timeout: 1.5, debounceTime: 0.05)
         
     var body: some View {
-        Group {
-            switch vm.state {
-            case .initializing, .loading, .altLoading:
-                HStack(spacing: 5) {
-                    ProgressView()
-                    if vm.state == .initializing || vm.state == .loading {
-                        Text("Fetching...")
-                    }
-                    else if vm.state == .altLoading {
-                        Text("Trying more relays...")
-                    }
+        switch vm.state {
+        case .initializing, .loading, .altLoading:
+            HStack(spacing: 5) {
+                ProgressView()
+                if vm.state == .initializing || vm.state == .loading {
+                    Text("Fetching...")
                 }
+                else if vm.state == .altLoading {
+                    Text("Trying more relays...")
+                }
+            }
+            .padding(10)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .onBecomingVisible { [weak vm, weak dim] in
+                guard let eventId = identifier.eventId else {
+                    vm?.error("Problem parsing nostr identifier")
+                    return
+                }
+                let fetchParams: FetchVM.FetchParams = (
+                    prio: true,
+                    req: { [weak vm, weak dim] taskId in
+                        bg().perform { [weak vm, weak dim] in // 1. CHECK LOCAL DB
+                            guard let vm, let dim else { return }
+                            if let event = Event.fetchEvent(id: eventId, context: bg()) {
+                                vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
+                            }
+                            else { // 2. ELSE CHECK RELAY
+                                req(RM.getEvent(id: eventId, subscriptionId: taskId))
+                            }
+                        }
+                    },
+                    onComplete: { [weak vm, weak dim] relayMessage, event in
+                        guard let vm, let dim else { return }
+                        if let event = event {
+                            vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
+                        }
+                        else if let event = Event.fetchEvent(id: eventId, context: bg()) { // 3. WE FOUND IT ON RELAY
+                            if vm.state == .altLoading, let relay = identifier.relays.first {
+#if DEBUG
+                                L.og.debug("Event found on using relay hint: \(eventId) - \(relay)")
+#endif
+                            }
+                            vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
+                        }
+                        // Still don't have the event? try to fetch from relay hint
+                        // TODO: Should try a relay we don't already have in our relay set
+                        else if (settings.followRelayHints && vpnGuardOK()) && [.initializing, .loading].contains(vm.state) {
+                            // try search relays and relay hint
+                            vm.altFetch()
+                        }
+                        else { // 5. TIMEOUT
+                            vm.timeout()
+                        }
+                    },
+                    altReq: { taskId in // IF WE HAVE A RELAY HINT WE USE THIS REQ, TRIGGERED BY vm.altFetch()
+                        // Try search relays
+                        req(RM.getEvent(id: eventId, subscriptionId: taskId), relayType: .SEARCH)
+                        guard let relay = identifier.relays.first else { return }
+                        
+#if DEBUG
+                        L.og.debug("FetchVM.3 HINT \(eventId)")
+#endif
+                        ConnectionPool.shared.sendEphemeralMessage(
+                            RM.getEvent(id: eventId, subscriptionId: taskId),
+                            relay: relay
+                        )
+                    }
+                    
+                )
+                vm?.setFetchParams(fetchParams)
+                vm?.fetch()
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(theme.lineColor, lineWidth: 1)
+            )
+        case .ready(let nrPost):
+            KindResolver(nrPost: nrPost, fullWidth: fullWidth, hideFooter: true, isDetail: false, isEmbedded: true, theme: theme)
+
+        case .timeout:
+            Text("Unable to fetch content")
                 .padding(10)
                 .frame(maxWidth: .infinity, alignment: .center)
-                .onBecomingVisible { [weak vm, weak dim] in
-                    guard let eventId = identifier.eventId else {
-                        vm?.error("Problem parsing nostr identifier")
-                        return
-                    }
-                    let fetchParams: FetchVM.FetchParams = (
-                        prio: true,
-                        req: { [weak vm, weak dim] taskId in
-                            bg().perform { [weak vm, weak dim] in // 1. CHECK LOCAL DB
-                                guard let vm, let dim else { return }
-                                if let event = Event.fetchEvent(id: eventId, context: bg()) {
-                                    vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
-                                }
-                                else { // 2. ELSE CHECK RELAY
-                                    req(RM.getEvent(id: eventId, subscriptionId: taskId))
-                                }
-                            }
-                        },
-                        onComplete: { [weak vm, weak dim] relayMessage, event in
-                            guard let vm, let dim else { return }
-                            if let event = event {
-                                vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
-                            }
-                            else if let event = Event.fetchEvent(id: eventId, context: bg()) { // 3. WE FOUND IT ON RELAY
-                                if vm.state == .altLoading, let relay = identifier.relays.first {
-#if DEBUG
-                                    L.og.debug("Event found on using relay hint: \(eventId) - \(relay)")
-#endif
-                                }
-                                vm.ready(NRPost(event: event, withFooter: false, isScreenshot: dim.isScreenshot))
-                            }
-                            // Still don't have the event? try to fetch from relay hint
-                            // TODO: Should try a relay we don't already have in our relay set
-                            else if (settings.followRelayHints && vpnGuardOK()) && [.initializing, .loading].contains(vm.state) {
-                                // try search relays and relay hint
-                                vm.altFetch()
-                            }
-                            else { // 5. TIMEOUT
-                                vm.timeout()
-                            }
-                        },
-                        altReq: { taskId in // IF WE HAVE A RELAY HINT WE USE THIS REQ, TRIGGERED BY vm.altFetch()
-                            // Try search relays
-                            req(RM.getEvent(id: eventId, subscriptionId: taskId), relayType: .SEARCH)
-                            guard let relay = identifier.relays.first else { return }
-                            
-#if DEBUG
-                            L.og.debug("FetchVM.3 HINT \(eventId)")
-#endif
-                            ConnectionPool.shared.sendEphemeralMessage(
-                                RM.getEvent(id: eventId, subscriptionId: taskId),
-                                relay: relay
-                            )
-                        }
-                        
-                    )
-                    vm?.setFetchParams(fetchParams)
-                    vm?.fetch()
-                }
                 .overlay(
                     RoundedRectangle(cornerRadius: 8)
                         .stroke(theme.lineColor, lineWidth: 1)
                 )
-            case .ready(let nrPost):
-                KindResolver(nrPost: nrPost, fullWidth: fullWidth, hideFooter: true, isDetail: false, isEmbedded: true, theme: theme)
-
-            case .timeout:
-                Text("Unable to fetch content")
-                    .padding(10)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(theme.lineColor, lineWidth: 1)
-                    )
-                
-            case .error(let error):
-                Text(error)
-                    .padding(10)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(theme.lineColor, lineWidth: 1)
-                    )
-            }
+            
+        case .error(let error):
+            Text(error)
+                .padding(10)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(theme.lineColor, lineWidth: 1)
+                )
         }
     }
 }
--- Nostur/Post/ContentRendererParts/NaddrView.swift
+++ Nostur/Post/ContentRendererParts/NaddrView.swift
@@ -21,112 +21,110 @@ struct NaddrView: View {
     @StateObject private var vm = FetchVM<NRPost>(timeout: 1.5, debounceTime: 0.05)
     
     var body: some View {
-        VStack {
-            switch vm.state {
-            case .initializing, .loading, .altLoading:
-                HStack(spacing: 5) {
-                    ProgressView()
-                    if vm.state == .initializing || vm.state == .loading {
-                        Text("Fetching...")
-                    }
-                    else if vm.state == .altLoading {
-                        Text("Trying more relays...")
-                    }
+        switch vm.state {
+        case .initializing, .loading, .altLoading:
+            HStack(spacing: 5) {
+                ProgressView()
+                if vm.state == .initializing || vm.state == .loading {
+                    Text("Fetching...")
                 }
-                .padding(10)
-                .frame(maxWidth: .infinity, alignment: .center)
-                .onBecomingVisible { [weak vm] in
-                    guard let naddr = try? ShareableIdentifier(naddr1),
-                            let kind = naddr.kind,
-                            let pubkey = naddr.pubkey,
-                            let definition = naddr.eventId
-                    else {
-                        return
-                    }
-                    
-                    
-                    let fetchParams: FetchVM.FetchParams = (
-                        prio: true,
-                        req: { [weak vm] taskId in
-                            bg().perform { [weak vm] in // 1. CHECK LOCAL DB
-                                guard let vm else { return }
-                                if let event = Event.fetchReplacableEvent(kind,
+                else if vm.state == .altLoading {
+                    Text("Trying more relays...")
+                }
+            }
+            .padding(10)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .onBecomingVisible { [weak vm] in
+                guard let naddr = try? ShareableIdentifier(naddr1),
+                        let kind = naddr.kind,
+                        let pubkey = naddr.pubkey,
+                        let definition = naddr.eventId
+                else {
+                    return
+                }
+                
+                
+                let fetchParams: FetchVM.FetchParams = (
+                    prio: true,
+                    req: { [weak vm] taskId in
+                        bg().perform { [weak vm] in // 1. CHECK LOCAL DB
+                            guard let vm else { return }
+                            if let event = Event.fetchReplacableEvent(kind,
+                                                                        pubkey: pubkey,
+                                                                        definition: definition,
+                                                                        context: bg()) {
+                                vm.ready(NRPost(event: event))
+                            }
+                            else { // 2. ELSE CHECK RELAY
+                                req(RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId))
+                        
+                            }
+                        }
+                    },
+                    onComplete: { [weak vm] relayMessage, event in
+                        guard let vm else { return }
+                        if let event = event {
+                            vm.ready(NRPost(event: event))
+                        }
+                        else if let event = Event.fetchReplacableEvent(kind,
                                                                             pubkey: pubkey,
                                                                             definition: definition,
-                                                                            context: bg()) {
-                                    vm.ready(NRPost(event: event))
-                                }
-                                else { // 2. ELSE CHECK RELAY
-                                    req(RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId))
-                            
-                                }
+                                                                            context: bg()) { // 3. WE FOUND IT ON RELAY
+                            if vm.state == .altLoading, let relay = naddr.relays.first {
+                                L.og.debug("Event found on using relay hint: \(event.id) - \(relay)")
                             }
-                        },
-                        onComplete: { [weak vm] relayMessage, event in
-                            guard let vm else { return }
-                            if let event = event {
-                                vm.ready(NRPost(event: event))
-                            }
-                            else if let event = Event.fetchReplacableEvent(kind,
-                                                                                pubkey: pubkey,
-                                                                                definition: definition,
-                                                                                context: bg()) { // 3. WE FOUND IT ON RELAY
-                                if vm.state == .altLoading, let relay = naddr.relays.first {
-                                    L.og.debug("Event found on using relay hint: \(event.id) - \(relay)")
-                                }
-                                vm.ready(NRPost(event: event))
-                            }
-                            // Still don't have the event? try to fetch from relay hint
-                            // TODO: Should try a relay we don't already have in our relay set
-                            else if (settings.followRelayHints && vpnGuardOK()) && [.initializing, .loading].contains(vm.state) {
-                                // try search relays and relay hint
-                                vm.altFetch()
-                            }
-                            else { // 5. TIMEOUT
-                                vm.timeout()
-                            }
-                        },
-                        altReq: { taskId in // IF WE HAVE A RELAY HINT WE USE THIS REQ, TRIGGERED BY vm.altFetch()
-                            // Try search relays
-                            req(RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId), relayType: .SEARCH)
-                            guard let relay = naddr.relays.first else { return }
-                            
-                            L.og.debug("FetchVM.3 HINT \(relay)")
-                            ConnectionPool.shared.sendEphemeralMessage(
-                                RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId),
-                                relay: relay
-                            )
+                            vm.ready(NRPost(event: event))
                         }
+                        // Still don't have the event? try to fetch from relay hint
+                        // TODO: Should try a relay we don't already have in our relay set
+                        else if (settings.followRelayHints && vpnGuardOK()) && [.initializing, .loading].contains(vm.state) {
+                            // try search relays and relay hint
+                            vm.altFetch()
+                        }
+                        else { // 5. TIMEOUT
+                            vm.timeout()
+                        }
+                    },
+                    altReq: { taskId in // IF WE HAVE A RELAY HINT WE USE THIS REQ, TRIGGERED BY vm.altFetch()
+                        // Try search relays
+                        req(RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId), relayType: .SEARCH)
+                        guard let relay = naddr.relays.first else { return }
                         
-                    )
-                    vm?.setFetchParams(fetchParams)
-                    vm?.fetch()
-                }
+                        L.og.debug("FetchVM.3 HINT \(relay)")
+                        ConnectionPool.shared.sendEphemeralMessage(
+                            RM.getArticle(pubkey: pubkey, kind:Int(kind), definition: definition, subscriptionId: taskId),
+                            relay: relay
+                        )
+                    }
+                    
+                )
+                vm?.setFetchParams(fetchParams)
+                vm?.fetch()
+            }
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(theme.lineColor, lineWidth: 1)
+            )
+        case .ready(let nrPost):
+            KindResolver(nrPost: nrPost, fullWidth: fullWidth, hideFooter: true, isDetail: false, isEmbedded: true, theme: theme)
+            
+        case .timeout:
+            Text("Unable to fetch content")
+                .padding(10)
+                .frame(maxWidth: .infinity, alignment: .center)
                 .overlay(
                     RoundedRectangle(cornerRadius: 8)
                         .stroke(theme.lineColor, lineWidth: 1)
                 )
-            case .ready(let nrPost):
-                KindResolver(nrPost: nrPost, fullWidth: fullWidth, hideFooter: true, isDetail: false, isEmbedded: true, theme: theme)
-                
-            case .timeout:
-                Text("Unable to fetch content")
-                    .padding(10)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(theme.lineColor, lineWidth: 1)
-                    )
-                
-            case .error(let error):
-                Text(error)
-                    .padding(10)
-                    .frame(maxWidth: .infinity, alignment: .center)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(theme.lineColor, lineWidth: 1)
-                    )
-            }
+            
+        case .error(let error):
+            Text(error)
+                .padding(10)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(theme.lineColor, lineWidth: 1)
+                )
         }
     }
 }
