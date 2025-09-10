8c735f43f12e17685cfbe8478edc0f422ef02582
Fabian <fabian@nostur.com>
2025-09-10 14:35:52 +0200

    Clean up duplicate save / bgSave / viewContextSave mess (2)

--- Nostur/CoreData/DataProvider.swift
+++ Nostur/CoreData/DataProvider.swift
@@ -6,8 +6,25 @@
 //
 
 import CoreData
+import Combine
 
 class DataProvider: ObservableObject {
+    
+    var subscriptions = Set<AnyCancellable>()
+    
+    // Debounced save to disk, 8 seconds.
+    public func saveToDisk(_ saveContext: SaveContext = .all, completion: (() -> Void)? = nil) {
+#if DEBUG
+        L.og.debug("💾💾 saveToDisk requested -[LOG]-")
+#endif
+        self.saveToDiskSubject.send((saveContext, completion))
+    }
+    
+    public func saveToDiskNow(_ saveContext: SaveContext = .all, completion: (() -> Void)? = nil) {
+        self._saveToDisk(saveContext, completion: completion)
+    }
+    
+    private var saveToDiskSubject = PassthroughSubject<(SaveContext, (() -> Void)?), Never>()
     
     public var bgStored: NSManagedObjectContext? // IMPORTING, PREPARING, TRANSFORMING FOR VIEW
     
@@ -25,6 +42,13 @@ class DataProvider: ObservableObject {
     
     private init(inMemory: Bool = false) {
         self.inMemory = inMemory
+        
+        saveToDiskSubject
+            .debounce(for: .seconds(8.0), scheduler: RunLoop.main)
+            .sink { [unowned self] (saveContextType, completion) in
+                self._saveToDisk(saveContextType, completion: completion)
+            }
+            .store(in: &subscriptions)
     }
     
     var databaseProblem = false
@@ -137,42 +161,24 @@ class DataProvider: ObservableObject {
         return taskContext
     }
     
-    func save(_ completion: (() -> Void)? = nil) { // TODO: replace all viewContext.save() with this save
+    private func _saveToDisk(_ saveContextType: SaveContext, completion: (() -> Void)? = nil) { // TODO: replace all viewContext.save() with this save
 #if DEBUG
-        L.og.debug("💾💾 DataProvider.shared().save() -[LOG]-")
+        L.og.debug("💾💾 DataProvider.shared()_saveToDisk -[LOG]-")
 #endif
-        
-        let bg = self.bgStored ?? self.bg
-     
-        
-        bg.perform { [weak self] in
-            guard let self = self else { return }
-#if DEBUG
-            L.og.debug("💾💾 BG: Registered objects: \(bg.registeredObjects.count) -[LOG]-")
-#endif
-            
-            if bg.hasChanges {
-                try? bg.save()
-#if DEBUG
-                L.og.debug("💾💾 🟢🟢 bg saved")
-#endif
-            }
-            self.container.viewContext.perform {
-#if DEBUG
-                L.og.debug("💾💾 VIEWCONTEXT: Registered objects: \(self.container.viewContext.registeredObjects.count) -[LOG]-")
-#endif
-                if self.container.viewContext.hasChanges {
-                    try? self.container.viewContext.save()
-#if DEBUG
-                    L.og.debug("💾💾💾💾 Saved to disk / iCloud 💾💾💾💾 -[LOG]-")
-#endif
-                }
-                completion?()
+        if saveContextType == .viewContext {
+            self._viewContextSave(completion)
+        }
+        else if saveContextType == .bgContext {
+            self._bgContextSave(completion)
+        }
+        else if saveContextType == .all {
+            self._bgContextSave {
+                self._viewContextSave(completion)
             }
         }
     }
     
-    func bgSave() {
+    private func _bgContextSave(_ completion: (() -> Void)? = nil) {
         let bg = self.bgStored ?? self.bg
         
         if Thread.isMainThread {
@@ -188,6 +194,8 @@ class DataProvider: ObservableObject {
                         L.og.error("🔴🔴 Could not save bgContext \(error)")
                     }
                 }
+                
+                completion?()
             }
         }
         else {
@@ -204,11 +212,25 @@ class DataProvider: ObservableObject {
 #endif
                 }
             }
+            completion?()
         }
-        
-        
     }
     
+    private func _viewContextSave(_ completion: (() -> Void)? = nil) {
+        self.container.viewContext.perform {
+#if DEBUG
+            L.og.debug("💾💾 VIEWCONTEXT: Registered objects: \(self.container.viewContext.registeredObjects.count) -[LOG]-")
+#endif
+            if self.container.viewContext.hasChanges {
+                try? self.container.viewContext.save()
+#if DEBUG
+                L.og.debug("💾💾💾💾 Saved to disk / iCloud 💾💾💾💾 -[LOG]-")
+#endif
+            }
+            completion?()
+        }
+    }
+
     // 254    468.00 ms    0.9%    254.00 ms                static DataProvider.shared()
     static func shared() -> DataProvider {
         // Use an in-memory store for previews
@@ -219,6 +241,12 @@ class DataProvider: ObservableObject {
         #endif
         return DataProvider.live
     }
+    
+    public enum SaveContext {
+        case bgContext
+        case viewContext
+        case all
+    }
 }
 
 func viewContext() -> NSManagedObjectContext {
@@ -227,44 +255,6 @@ func viewContext() -> NSManagedObjectContext {
 
 func bg() -> NSManagedObjectContext {
     (DataProvider.shared().bgStored ?? DataProvider.shared().bg) // .bg lazy computed, so may have thread contention issues when used a lot, try to directly access .bgStored here.
-}
-
-func viewContextSave() { // TODO make this always debounce + throttle latest
-    if DataProvider.shared().viewContext.hasChanges {
-        do {
-            try DataProvider.shared().viewContext.save()
-#if DEBUG
-            L.og.debug("💾💾💾💾 Saved to disk / iCloud 💾💾💾💾 -[LOG]-")
-#endif
-        }
-        catch {
-#if DEBUG
-            L.og.error("🔴🔴 Could not save() context \(error)")
-#endif
-        }
-    }
-}
-
-func bgSave() {
-    DataProvider.shared().bgSave()
-}
-
-func save(context: NSManagedObjectContext = context()) {
-    if context.hasChanges {
-        do {
-            try context.save()
-            if Thread.isMainThread {
-#if DEBUG
-                L.og.debug("💾💾💾💾 Saved to disk / iCloud 💾💾💾💾 -[LOG]-")
-#endif
-            }
-        }
-        catch {
-#if DEBUG
-            L.og.error("🔴🔴 Could not save() context \(error)")
-#endif
-        }
-    }
 }
 
 func context() -> NSManagedObjectContext {
