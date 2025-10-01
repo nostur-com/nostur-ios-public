7463aba8160380645cf5ae0a101b2da2a8396bae
Fabian <fabian@nostur.com>
2025-10-01 11:14:48 +0200

    Clean up

--- Nostur/Screens/MainTabs/Home/MainFeedsScreen.swift
+++ Nostur/Screens/MainTabs/Home/MainFeedsScreen.swift
@@ -63,44 +63,6 @@ struct MainFeedsScreen: View {
     @State private var backlog = Backlog(backlogDebugName: "MainFeedsScreen")
     
     @AppStorage("feed_emoji_type") var emojiType: String = "😂"
-    
-    private var navigationTitle: String {
-        if selectedSubTab == "List" {
-            return (selectedList?.name_ ?? String(localized: "List"))
-        }
-        if selectedSubTab == "Following" {
-            return String(localized: "Following", comment: "Tab title for feed of people you follow")
-        }
-        if selectedSubTab == "Picture" {
-            return String(localized: "Photos", comment: "Tab title for photos feed of people you follow")
-        }
-        
-        if selectedSubTab == "Explore" {
-            return String(localized: "Explore", comment: "Tab title for the Explore feed")
-        }
-        if selectedSubTab == "Emoji" {
-            return "Funny"
-        }
-        if selectedSubTab == "Zapped" {
-            return String(localized: "Zapped", comment: "Tab title for the Zapped feed")
-        }
-        if selectedSubTab == "Hot" {
-            return String(localized: "Hot", comment: "Tab title for the Hot feed")
-        }
-//        if selectedSubTab == "Discover" {
-//            return String(localized: "Discover", comment: "Tab title for the Discover feed")
-//        }
-        if selectedSubTab == "DiscoverLists" {
-            return String(localized: "Follow Packs & Lists", comment: "Tab title for the Discover Lists feed")
-        }
-        if selectedSubTab == "Gallery" {
-            return String(localized: "Gallery", comment: "Tab title for the Gallery feed")
-        }
-        if selectedSubTab == "Articles" {
-            return String(localized: "Reads", comment: "Tab title for the Reads (Articles) feed")
-        }
-        return String(localized: "Feed", comment: "Tab title for a feed")
-    }
 
     // If only the Following feed is enabled and all other feeds are disabled, we can hide the entire tab bar
     private var shouldHideTabBar: Bool {
@@ -426,7 +388,7 @@ struct MainFeedsScreen: View {
             createFollowingFeed(newAccount)
             createPictureFeed(newAccount)
         }
-        .navigationTitle(navigationTitle)
+        .navigationTitle(homeTabNavigationTitle(self.selectedList))
         .navigationBarTitleDisplayMode(.inline)
         .onReceive(lists.publisher.collect()) { lists in
             guard didCreate else { return } // Only update here after .onAppear { } has ran.
