d23ab67f625892247b871d4adf45d334cb737207
Fabian <fabian@nostur.com>
2025-11-05 19:12:06 +0100

    Add icons to desktop feed selector

--- Nostur/Screens/Layout/Mac/FeedSelector.swift
+++ Nostur/Screens/Layout/Mac/FeedSelector.swift
@@ -9,6 +9,23 @@ import SwiftUI
 import NavigationBackport
 
 extension CloudFeed {
+    func feedIconName() -> String {
+        switch self.feedType {
+            case .following(_):
+                return "person.circle"
+            case .picture(_):
+                return "photo"
+            case .pubkeys(_):
+                return "star"
+            case .relays(_):
+                return "star"
+            case .followSet(_), .followPack(_):
+                return "star"
+            default:
+                return "star"
+        }
+    }
+    
     func feedTitle() -> String {
         switch self.feedType {
             case .following(_):
@@ -28,7 +45,17 @@ extension CloudFeed {
 }
 
 struct ColumnConfigToolbarMenu: ViewModifier {
+    
+    @AppStorage("enable_zapped_feed") private var enableZappedFeed: Bool = true
+    @AppStorage("enable_hot_feed") private var enableHotFeed: Bool = true
+    @AppStorage("enable_picture_feed") private var enablePictureFeed: Bool = true
+    @AppStorage("enableEmojiFeed") private var enableEmojiFeed: Bool = true
+    @AppStorage("enable_discover_feed") private var enableDiscoverFeed: Bool = true
+    @AppStorage("enable_discover_lists_feed") private var enableDiscoverListsFeed: Bool = true
+    @AppStorage("enable_gallery_feed") private var enableGalleryFeed: Bool = true
+    @AppStorage("enable_article_feed") private var enableArticleFeed: Bool = true
     @AppStorage("enable_explore_feed") private var enableExploreFeed: Bool = true
+    
     let feeds: [CloudFeed]
     @Binding var columnType: MacColumnType
     var title: String = "Select Feed"
@@ -89,31 +116,47 @@ struct ColumnConfigToolbarMenu: ViewModifier {
     
     @ViewBuilder
     private var menuItems: some View {
-        Button("Hot") {
-            columnType = .hot
+        
+        if enableHotFeed {
+            Button("Hot", systemImage: "flame") {
+                columnType = .hot
+            }
+        }
+
+        if enableEmojiFeed {
+            Button { columnType = .emoji } label: {
+                Label {
+                    Text("Funny")
+                } icon: {
+                    Image("LaughterIcon")
+                        .renderingMode(.template)
+                }
+            }
         }
         
-        Button("Funny") {
-            columnType = .emoji
+        if enableZappedFeed {
+            Button("Zapped", systemImage: "bolt") {
+                columnType = .zapped
+            }
         }
         
-        Button("Zapped") {
-            columnType = .zapped
+        if enableArticleFeed {
+            Button("Reads", systemImage: "newspaper") {
+                columnType = .articles
+            }
         }
         
-        Button("Reads") {
-            columnType = .articles
+        if enableGalleryFeed {
+            Button("Gallery", systemImage: "photo.on.rectangle.angled") {
+                columnType = .gallery
+            }
         }
         
-        Button("Gallery") {
-            columnType = .gallery
-        }
-        
-        Button("Lists & Follow Packs") {
+        Button("Follow Packs & Lists", systemImage: "person.2.crop.square.stack") {
             columnType = .discoverLists
         }
         
-        Button("Notifications") {
+        Button("Notifications", systemImage: "bell") {
             columnType = .notifications(nil)
         }
         
@@ -126,7 +169,7 @@ struct ColumnConfigToolbarMenu: ViewModifier {
         Divider()
 
         ForEach(feeds) { feed in
-            Button(feed.feedTitle()) {
+            Button(feed.feedTitle(), systemImage: feed.feedIconName()) {
                 guard let feedId = feed.id?.uuidString else { return }
                 columnType = .cloudFeed(feedId)
             }
