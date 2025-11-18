14c95d140d8bc30eec11acf0113bda3788d5d999
Fabian <fabian@nostur.com>
2025-11-18 20:08:34 +0100

    WIP video

--- Nostur/Post/NR/Kinds/34235-VideoEvent/VideoEventView.swift
+++ Nostur/Post/NR/Kinds/34235-VideoEvent/VideoEventView.swift
@@ -412,6 +412,34 @@ func prefetchNextVideos(at index: Int, urls: [URL]) {
     }
 }
 
+@available(iOS 26.0, *)
+#Preview("Vine 4") {
+    @Previewable @Environment(\.theme) var theme
+    @Previewable @State var nrPost = testNRPost(###"{"pubkey":"5943c88f3c60cd9edb125a668e2911ad419fc04e94549ed96a721901dd958372","created_at":1763179480,"kind":22,"tags":[["alt","Vertical Video"],["title",""],["published_at","1763179481"],["imeta","url https://blossom.primal.net/cf5a5ff1dddc3b97d8938f33d1088c9e5babcdc3f94c5178112392e9b3a36d27.mp4","m video/mp4","alt Vertical Video","x cf5a5ff1dddc3b97d8938f33d1088c9e5babcdc3f94c5178112392e9b3a36d27","size 3069605","dim 720x1280","blurhash _FF}~1~p%z-p~W0fE2.S?aNH^+xu%gt79ZIV-WWVNaxu-:IpjG%MNHoMsAR,S6kCX5NxofxZNGf,W.slt7X8bFs:%1WARkxFslR*R*xDV@kDjFnOoft7WBs;t7oKafs;of"],["nonce","229","16"]],"id":"0000c09aa7133e2e75e7e352af918c56e7a8cafc4ed456a67a24dc4a9f777272","content":"Marin 💕✨\nOriginally published in: 2025-11-13","sig":"53badb2a42b0b147252b0ac23545773a50ec123c9761da6e910a7c37f046dccae51e7174d5498ff83e4a33987c9a618808b62d7c34b09abaafec4b543b6ed819"}"###)
+    PreviewContainer({ pe in
+        
+    }) {
+        PreviewApp {
+            ScrollView {
+                LazyVStack {
+//                    Color.random
+//                        .frame(height: 400)
+                    
+                    VideoPostLayout(nrPost: nrPost, theme: theme) {
+                        VideoFeedItemView(videoURL: URL(string: "https://cdn.divine.video/97a09d5ad5874c1234c2d4f8cab1001895582afbda3b48e97513b00f79cd54b7.mp4")!)
+                    }
+                    
+//                    Color.random
+//                        .frame(height: 400)
+//                    
+//                    Color.random
+//                        .frame(height: 400)
+                }
+            }
+        }
+    }
+}
+
 struct VideoPost: View {
     let nrPost: NRPost
     let theme: Theme
