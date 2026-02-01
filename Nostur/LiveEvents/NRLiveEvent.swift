6baad7b6b7be50b439ccf98769dc65fc9fe3a0fd
Fabian <fabian@nostur.com>
2026-02-01 18:51:10 +0100

    Increase oldest "live" stream to 12 hours ago before ignoring

--- Nostur/LiveEvents/NRLiveEvent.swift
+++ Nostur/LiveEvents/NRLiveEvent.swift
@@ -603,10 +603,10 @@ extension Event {
     
     func isLive() -> Bool {
         // remove old events that appear live but where we maybe missed receiving the "ended" state
-        // so only keep if $0.created_at is newer than 8 hours ago AND we have a "streaming" tag, should be enough sanity check
+        // so only keep if $0.created_at is newer than 12 hours ago AND we have a "streaming" tag, should be enough sanity check
         // (AND also "live")
         let createdAt = Date(timeIntervalSince1970: Double(self.created_at))
-        let eightHoursAgo = Date().addingTimeInterval(-8 * 60 * 60)
+        let eightHoursAgo = Date().addingTimeInterval(-12 * 60 * 60)
         if createdAt < eightHoursAgo && self.fastTags.contains(where: { $0.0 == "streaming" }) {
             return false
         }
